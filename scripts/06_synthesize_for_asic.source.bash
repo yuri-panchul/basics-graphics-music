a559acb352c5060cd13902a352da3fa1ed39a307
Yuri Panchul <yuri@panchul.com>
2023-07-08 18:05:46 -0700

    Consider OpenLane support sufficient for now on

--- scripts/06_synthesize_for_asic.source.bash
+++ scripts/06_synthesize_for_asic.source.bash
@@ -17,7 +17,7 @@ find .. "$lab_dir/common"  \
 
 #-----------------------------------------------------------------------------
 
-pushd "$openlane_dir"
+pushd "$openlane_dir" &> /dev/null
 make quick_run QUICK_RUN_DESIGN=$lab_name |& tee "$log"
 popd
 
@@ -33,28 +33,28 @@ last_run_dir=$(ls -d "$runs_dir"/RUN* | sort | tail -1)
 
 cp "$log" 01_main.log
 
-   cp "$last_run_dir"/results/synthesis/top.v               02_synthesis.v                           \
-&& cp "$last_run_dir"/results/placement/top.nl.v            03_placement.nl.v                        \
-&& cp "$last_run_dir"/results/routing/top.nl.v              04_routing.nl.v                          \
-&& cp "$last_run_dir"/results/final/verilog/gl/top.nl.v     05_final_no_power_grid.nl.v              \
-&& cp "$last_run_dir"/results/final/verilog/gl/top.v        06_final.v                               \
-&& cp "$last_run_dir"/reports/signoff/31-rcx_sta.max.rpt    07_static_timing_analysis.sta.max.rpt    \
-&& cp "$last_run_dir"/reports/signoff/31-rcx_sta.min.rpt    08_static_timing_analysis.sta.min.rpt    \
-&& cp "$last_run_dir"/reports/signoff/31-rcx_sta.power.rpt  09_static_timing_analysis.sta.power.rpt  \
-&& cp "$last_run_dir"/results/signoff/top.lef               10_library_exchange_format.lef           \
-&& cp "$last_run_dir"/results/signoff/top.mag               11_magic.mag                             \
-&& cp "$last_run_dir"/results/signoff/top.sdf               12_standard_delay_format.sdf             \
+   cp "$last_run_dir"/results/synthesis/top.v               asic_02_synthesis.v                           \
+&& cp "$last_run_dir"/results/placement/top.nl.v            asic_03_placement.nl.v                        \
+&& cp "$last_run_dir"/results/routing/top.nl.v              asic_04_routing.nl.v                          \
+&& cp "$last_run_dir"/results/final/verilog/gl/top.nl.v     asic_05_final_no_power_grid.nl.v              \
+&& cp "$last_run_dir"/results/final/verilog/gl/top.v        asic_06_final.v                               \
+&& cp "$last_run_dir"/reports/signoff/31-rcx_sta.max.rpt    asic_07_static_timing_analysis.sta.max.rpt    \
+&& cp "$last_run_dir"/reports/signoff/31-rcx_sta.min.rpt    asic_08_static_timing_analysis.sta.min.rpt    \
+&& cp "$last_run_dir"/reports/signoff/31-rcx_sta.power.rpt  asic_09_static_timing_analysis.sta.power.rpt  \
+&& cp "$last_run_dir"/results/signoff/top.lef               asic_10_library_exchange_format.lef           \
+&& cp "$last_run_dir"/results/signoff/top.mag               asic_11_magic.mag                             \
+&& cp "$last_run_dir"/results/signoff/top.sdf               asic_12_standard_delay_format.sdf             \
 || error "Cannot copy something"
 
 exit 0
 
-   cp "$last_run_dir"/results/synthesis/top.v                   02_synthesis.v    \
-&& cp "$last_run_dir"/results/placement/top.resized.v           03_placement.v    \
-&& cp "$last_run_dir"/results/cts/top.resized.v                 04_cts.v          \
-&& cp "$last_run_dir"/results/routing/top.resized.v             05_routing.v      \
-&& cp "$last_run_dir"/results/final/verilog/gl/top.v            06_final.v        \
-&& cp "$last_run_dir"/reports/signoff/29-rcx_mca_sta.rpt        07_mca_sta.rpt    \
-&& cp "$last_run_dir"/reports/signoff/29-rcx_mca_sta.area.rpt   08_mca_area.rpt   \
-&& cp "$last_run_dir"/reports/signoff/29-rcx_mca_sta.power.rpt  09_mca_power.rpt  \
-&& cp "$last_run_dir"/results/signoff/top.mag                   10_magic.mag      \
+   cp "$last_run_dir"/results/synthesis/top.v                   asic_02_synthesis.v    \
+&& cp "$last_run_dir"/results/placement/top.resized.v           asic_03_placement.v    \
+&& cp "$last_run_dir"/results/cts/top.resized.v                 asic_04_cts.v          \
+&& cp "$last_run_dir"/results/routing/top.resized.v             asic_05_routing.v      \
+&& cp "$last_run_dir"/results/final/verilog/gl/top.v            asic_06_final.v        \
+&& cp "$last_run_dir"/reports/signoff/29-rcx_mca_sta.rpt        asic_07_mca_sta.rpt    \
+&& cp "$last_run_dir"/reports/signoff/29-rcx_mca_sta.area.rpt   asic_08_mca_area.rpt   \
+&& cp "$last_run_dir"/reports/signoff/29-rcx_mca_sta.power.rpt  asic_09_mca_power.rpt  \
+&& cp "$last_run_dir"/results/signoff/top.mag                   asic_10_magic.mag      \
 || error "Cannot copy something"
