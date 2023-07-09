9a0795f86ac5cd67fa484aecc3291e4345104633
Yuri Panchul <yuri@panchul.com>
2023-07-09 10:20:04 -0700

    Added lab 05 seven segment letter

---
+++ labs/05_7seven_segment_letter/top.sv
@@ -0,0 +1,103 @@
+`include "config.svh"
+
+module top
+# (
+    parameter clk_mhz = 50,
+              w_key   = 4,
+              w_sw    = 8,
+              w_led   = 8,
+              w_digit = 8,
+              w_gpio  = 20
+)
+(
+    input                        clk,
+    input                        rst,
+
+    // Keys, switches, LEDs
+
+    input        [w_key   - 1:0] key,
+    input        [w_sw    - 1:0] sw,
+    output logic [w_led   - 1:0] led,
+
+    // A dynamic seven-segment display
+
+    output logic [          7:0] abcdefgh,
+    output logic [w_digit - 1:0] digit,
+
+    // VGA
+
+    output logic                 vsync,
+    output logic                 hsync,
+    output logic [          3:0] red,
+    output logic [          3:0] green,
+    output logic [          3:0] blue,
+
+    input        [         23:0] mic,
+
+    // General-purpose Input/Output
+
+    inout  logic [w_gpio  - 1:0] gpio
+);
+
+    //------------------------------------------------------------------------
+
+       assign led      = '0;
+    // assign abcdefgh = '0;
+    // assign digit    = '0;
+       assign vsync    = '0;
+       assign hsync    = '0;
+       assign red      = '0;
+       assign green    = '0;
+       assign blue     = '0;
+
+    //------------------------------------------------------------------------
+
+    //   --a--
+    //  |     |
+    //  f     b
+    //  |     |
+    //   --g--
+    //  |     |
+    //  e     c
+    //  |     |
+    //   --d--  h
+
+    typedef enum bit [7:0]
+    {
+        F     = 8'b1000_1110,
+        P     = 8'b1100_1110,
+        G     = 8'b1011_1100,
+        A     = 8'b1110_1110,
+        space = 8'b0000_0000
+    }
+    seven_seg_encoding_e;
+
+    assign abcdefgh = key [0] ? P : F;
+    assign digit    = w_digit' (key [1] ? 2'b10 : 2'b01);
+
+    // Exercise 1: Display the first letters
+    // of your first name and last name instead.
+
+    // assign abcdefgh = ...
+    // assign digit    = ...
+
+    // Exercise 2: Display letters of a 4-character word
+    // using this code to display letter of FPGA as an example
+
+    /*
+    seven_seg_encoding_e letter;
+
+    always_comb
+      case (4' (key))
+      4'b1000: letter = F;
+      4'b0100: letter = P;
+      4'b0010: letter = G;
+      4'b0001: letter = A;
+      default: letter = space;
+      endcase
+
+    assign abcdefgh = letter;
+    assign digit    = w_digit' (key);
+    */
+
+endmodule
