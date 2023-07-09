a64bf3457657ea1aae3b1a3d57c52ac2cd30bf31
Yuri Panchul <yuri@panchul.com>
2023-07-08 17:41:13 -0700

    Developing OpenLane synthesis script

--- scripts/06_synthesize_for_asic.source.bash
+++ scripts/06_synthesize_for_asic.source.bash
@@ -17,8 +17,9 @@ find .. "$lab_dir/common"  \
 
 #-----------------------------------------------------------------------------
 
-cd "$openlane_dir"
+pushd "$openlane_dir"
 make quick_run QUICK_RUN_DESIGN=$lab_name |& tee "$log"
+popd
 
 #-----------------------------------------------------------------------------
 
@@ -31,6 +32,21 @@ last_run_dir=$(ls -d "$runs_dir"/RUN* | sort | tail -1)
 ! [ -z "${last_run_dir-}" ] || error "No RUN directory"
 
 cp "$log" 01_main.log
+
+   cp "$last_run_dir"/results/synthesis/top.v               02_synthesis.v                           \
+&& cp "$last_run_dir"/results/placement/top.nl.v            03_placement.nl.v                        \
+&& cp "$last_run_dir"/results/routing/top.nl.v              04_routing.nl.v                          \
+&& cp "$last_run_dir"/results/final/verilog/gl/top.nl.v     05_final_no_power_grid.nl.v              \
+&& cp "$last_run_dir"/results/final/verilog/gl/top.v        06_final.v                               \
+&& cp "$last_run_dir"/reports/signoff/31-rcx_sta.max.rpt    07_static_timing_analysis.sta.max.rpt    \
+&& cp "$last_run_dir"/reports/signoff/31-rcx_sta.min.rpt    08_static_timing_analysis.sta.min.rpt    \
+&& cp "$last_run_dir"/reports/signoff/31-rcx_sta.power.rpt  09_static_timing_analysis.sta.power.rpt  \
+&& cp "$last_run_dir"/results/signoff/top.lef               10_library_exchange_format.lef           \
+&& cp "$last_run_dir"/results/signoff/top.mag               11_magic.mag                             \
+&& cp "$last_run_dir"/results/signoff/top.sdf               12_standard_delay_format.sdf             \
+|| error "Cannot copy something"
+
+exit 0
 
    cp "$last_run_dir"/results/synthesis/top.v                   02_synthesis.v    \
 && cp "$last_run_dir"/results/placement/top.resized.v           03_placement.v    \
