12b70d60c5c46bd4f7a9c0d3451dac68fd4231e4
Yuri Panchul <yuri@panchul.com>
2023-08-14 00:08:17 -0700

    Cosmetic script improvements

--- scripts/steps/04_configure_fpga.source.bash
+++ scripts/steps/04_configure_fpga.source.bash
@@ -2,13 +2,7 @@
 . "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/00_setup.source.bash"
 
 case $fpga_toolchain in
-    quartus)
-        configure_fpga_quartus
-        ;;
-    gowin)
-        configure_fpga_gowin
-        ;;
-    *)
-        error "Unsupported FPGA synthesis toolchain: $fpga_toolchain"
-        ;;
+    quartus ) configure_fpga_quartus ;;
+    gowin   ) configure_fpga_gowin   ;;
+    *       ) error "Unsupported FPGA synthesis toolchain: $fpga_toolchain" ;;
 esac
