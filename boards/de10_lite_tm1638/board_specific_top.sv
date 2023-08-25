8e2d7871d274547c1d8e3543f52f179f303793b6
Yuri Panchul <yuri@panchul.com>
2023-08-25 14:18:32 -0700

    Added a tm1638 extension to de10_lite

---
+++ boards/de10_lite_tm1638/board_specific_top.sv
@@ -0,0 +1,210 @@
+`define DUPLICATE_TM_SIGNALS_WITH_REGULAR
+// `define CONCAT_TM_SIGNALS_AND_REGULAR
+// `define CONCAT_REGULAR_SIGNALS_AND_TM
+
+`ifndef DUPLICATE_TM_SIGNALS_WITH_REGULAR
+    `define ADD_REGULAR_AND_TM_SIGNAL_WIDTH
+`endif
+
+module board_specific_top
+# (
+    parameter clk_mhz = 50,
+              w_key   = 2,
+              w_sw    = 9,  // One sw is used as a reset
+              w_led   = 10,
+              w_digit = 6,
+              w_gpio  = 36
+)
+(
+    input                   MAX10_CLK1_50,
+
+    input  [w_key    - 1:0] KEY,
+    input  [w_sw + 1 - 1:0] SW,  // One sw is used as a reset
+    output [w_led    - 1:0] LEDR,
+
+    output [           7:0] HEX0,
+    output [           7:0] HEX1,
+    output [           7:0] HEX2,
+    output [           7:0] HEX3,
+    output [           7:0] HEX4,
+    output [           7:0] HEX5,
+
+    output                  VGA_HS,
+    output                  VGA_VS,
+    output [           3:0] VGA_R,
+    output [           3:0] VGA_G,
+    output [           3:0] VGA_B,
+
+    inout  [w_gpio   - 1:0] GPIO
+);
+
+    //------------------------------------------------------------------------
+
+    wire              clk = MAX10_CLK1_50;
+
+    wire              rst = SW [w_sw];
+    wire [w_sw - 1:0] sw  = SW [w_sw - 1:0];
+
+    //------------------------------------------------------------------------
+
+    wire [          7:0] abcdefgh;
+    wire [w_digit - 1:0] digit;
+
+    wire [         23:0] mic;
+
+    //------------------------------------------------------------------------
+
+    localparam w_tm_key    = 8,
+               w_tm_led    = 8,
+               w_tm_digit  = 8;
+
+    //------------------------------------------------------------------------
+
+    `ifdef DUPLICATE_TM_SIGNALS_WITH_REGULAR
+
+        localparam w_top_key   = w_tm_key   > w_key   ? w_tm_key   : w_key,
+                   w_top_led   = w_tm_led   > w_led   ? w_tm_led   : w_led,
+                   w_top_digit = w_tm_digit > w_digit ? w_tm_digit : w_digit;
+
+    `else  // Concatenate the signals
+
+        localparam w_top_key   = w_tm_key   + w_key   ;
+                   w_top_led   = w_tm_led   + w_led   ;
+                   w_top_digit = w_tm_digit + w_digit ;
+    `endif
+
+    //------------------------------------------------------------------------
+
+    wire  [w_tm_key    - 1:0] tm_key;
+    wire  [w_tm_led    - 1:0] tm_led;
+    wire  [w_tm_digit  - 1:0] tm_digit;
+
+    logic [w_top_key   - 1:0] top_key;
+    wire  [w_top_led   - 1:0] top_led;
+    wire  [w_top_digit - 1:0] top_digit;
+
+    //------------------------------------------------------------------------
+
+    `ifdef CONCAT_TM_SIGNALS_AND_REGULAR
+
+        assign top_key = { tm_key, ~ KEY };
+
+        assign { tm_led   , LEDR  } = top_led;
+        assign { tm_digit , digit } = top_digit;
+
+    `elif CONCAT_REGULAR_SIGNALS_AND_TM
+
+        assign top_key = { ~ KEY, tm_key };
+
+        assign { LEDR  , tm_led   } = top_led;
+        assign { digit , tm_digit } = top_digit;
+
+    `else  // DUPLICATE_TM_SIGNALS_WITH_REGULAR
+
+        always_comb
+        begin
+            top_key = '0;
+
+            top_key [w_key    - 1:0] |= ~ KEY;
+            top_key [w_tm_key - 1:0] |= tm_key;
+        end
+
+        assign LEDR     = top_led   [w_led      - 1:0];
+        assign tm_led   = top_led   [w_tm_led   - 1:0];
+
+        assign digit    = top_digit [w_digit    - 1:0];
+        assign tm_digit = top_digit [w_tm_digit - 1:0];
+
+    `endif
+
+    //------------------------------------------------------------------------
+
+    top
+    # (
+        .clk_mhz ( clk_mhz     ),
+        .w_key   ( w_top_key   ),
+        .w_sw    ( w_sw        ),
+        .w_led   ( w_top_led   ),
+        .w_digit ( w_top_digit ),
+        .w_gpio  ( w_gpio      )
+    )
+    i_top
+    (
+        .clk      ( clk       ),
+        .rst      ( rst       ),
+
+        .key      ( top_key   ),
+        .sw       ( sw        ),
+
+        .led      ( top_led   ),
+
+        .abcdefgh ( abcdefgh  ),
+        .digit    ( top_digit ),
+
+        .vsync    ( VGA_VS    ),
+        .hsync    ( VGA_HS    ),
+
+        .red      ( VGA_R     ),
+        .green    ( VGA_G     ),
+        .blue     ( VGA_B     ),
+
+        .mic      ( mic       ),
+        .gpio     ( GPIO      )
+    );
+
+    //------------------------------------------------------------------------
+
+    wire [$left (abcdefgh):0] hgfedcba;
+
+    generate
+        genvar i;
+
+        for (i = 0; i < $bits (abcdefgh); i ++)
+        begin : abc
+            assign hgfedcba [i] = abcdefgh [$left (abcdefgh) - i];
+        end
+    endgenerate
+
+    assign HEX0 = digit [0] ? ~ hgfedcba : '1;
+    assign HEX1 = digit [1] ? ~ hgfedcba : '1;
+    assign HEX2 = digit [2] ? ~ hgfedcba : '1;
+    assign HEX3 = digit [3] ? ~ hgfedcba : '1;
+    assign HEX4 = digit [4] ? ~ hgfedcba : '1;
+    assign HEX5 = digit [5] ? ~ hgfedcba : '1;
+
+   //------------------------------------------------------------------------
+
+    tm1638_board_controller
+    # (
+        .w_digit ( w_digit )
+    )
+    i_ledkey
+    (
+        .clk      (    clk                  ), // 50 MHz
+        .rst      (    tm1638_rst           ), // Don't make reset tm1638_board_controller by tm1638_key
+        .hgfedcba (    hgfedcba             ),
+        .digit    (    digit                ),
+        .ledr     (    led [w_tm_led - 1:0] ),
+        .keys     (    tm_key               ), // S8 key reserved for reset
+        .sio_clk  (    GPIO_0 [31]          ),
+        .sio_stb  (    GPIO_0 [33]          ),
+        .sio_data (    GPIO_0 [35]          )
+    );
+
+    //------------------------------------------------------------------------
+
+    inmp441_mic_i2s_receiver i_microphone
+    (
+        .clk   ( clk      ),
+        .rst   ( rst      ),
+        .lr    ( GPIO [5] ),
+        .ws    ( GPIO [3] ),
+        .sck   ( GPIO [1] ),
+        .sd    ( GPIO [0] ),
+        .value ( mic      )
+    );
+
+    assign GPIO [4] = 1'b0;  // GND
+    assign GPIO [2] = 1'b1;  // VCC
+
+endmodule
