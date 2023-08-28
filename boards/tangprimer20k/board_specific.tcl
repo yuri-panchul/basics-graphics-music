73b4444a708131741b6c382a151c22c36740ed70
Yuri Panchul <yuri@panchul.com>
2023-08-28 15:09:25 -0700

    Corrected GoWin tcl file for Educational edition

--- boards/tangprimer20k/board_specific.tcl
+++ boards/tangprimer20k/board_specific.tcl
@@ -1,4 +1,5 @@
-set_device GW2A-LV18PG256C8/I7 -device_version NA
+# set_device GW2A-LV18PG256C8/I7 -device_version NA
+set_device GW2A-LV18PG256C8/I7
 set_option -synthesis_tool gowinsynthesis
 set_option -output_base_name fpga_project
 set_option -top_module board_specific_top
@@ -6,4 +7,4 @@ set_option -verilog_std sysv2017
 set_option -use_sspi_as_gpio 1
 set_option -use_ready_as_gpio 1
 set_option -use_done_as_gpio 1
-set_option -cmser_mode auto
+# set_option -cmser_mode auto
