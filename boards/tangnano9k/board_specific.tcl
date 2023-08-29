c0f4412f00d78118d7e24f19b9c891fa163c8bdc
Aleksandr Ryabov <a.ryabov@innopolis.university>
2023-08-29 18:36:24 +0300

    Added board files for Sipeed Tang Nano 9k

---
+++ boards/tangnano9k/board_specific.tcl
@@ -0,0 +1,5 @@
+set_device GW1NR-LV9QN88PC6/I5
+set_option -synthesis_tool gowinsynthesis
+set_option -output_base_name fpga_project
+set_option -top_module board_specific_top
+set_option -verilog_std sysv2017
