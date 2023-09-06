2e80335b7bef6312d0b7a33025024f9e93b8fd60
Yuri Panchul <yuri@panchul.com>
2023-09-05 19:42:14 -0700

    Added write_checkpoint

--- scripts/fpga/xilinx_vivado_synthesize.tcl
+++ scripts/fpga/xilinx_vivado_synthesize.tcl
@@ -8,25 +8,29 @@ read_xdc "../../../boards/$fpga_board/board_specific.xdc"
 
 synth_design -include_dirs ../../common -part $part -top board_specific_top
 
-report_timing_summary -file post_synth_timing_summary.rpt
-report_power          -file post_synth_power.rpt
+write_checkpoint          -force post_synth
+report_timing_summary     -file  post_synth_timing_summary.rpt
+report_power              -file  post_synth_power.rpt
 
 opt_design
+power_opt_design
 place_design
 phys_opt_design
 
-report_timing_summary -file post_place_timing_summary.rpt
+write_checkpoint          -force post_place
+report_timing_summary     -file  post_place_timing_summary.rpt
 
 route_design
 
-report_timing_summary    -file post_route_timing_summary.rpt
-report_timing            -sort_by group -max_paths 100 -path_type summary -file post_route_timing.rpt
-report_clock_utilization -file clock_util.rpt
-report_utilization       -file post_route_util.rpt
-report_power             -file post_route_power.rpt
-report_drc               -file post_imp_drc.rpt
+write_checkpoint          -force post_route
+report_timing_summary     -file  post_route_timing_summary.rpt
+report_timing             -sort_by group -max_paths 100 -path_type summary -file post_route_timing.rpt
+report_clock_utilization  -file  clock_util.rpt
+report_utilization        -file  post_route_util.rpt
+report_power              -file  post_route_power.rpt
+report_drc                -file  post_imp_drc.rpt
 
-write_verilog                -force bft_impl_netlist.v
-write_xdc     -no_fixed_only -force bft_impl.xdc
+write_verilog             -force bft_impl_netlist.v
+write_xdc -no_fixed_only  -force bft_impl.xdc
 
-write_bitstream -force fpga_project.bit
+write_bitstream           -force fpga_project.bit
