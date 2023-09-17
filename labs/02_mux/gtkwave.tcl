d1fdc85ef9711a9cc0868417a5779ed475392b7c
Yuri Panchul <yuri@panchul.com>
2023-09-17 15:09:50 -0700

    Added tcl file for GTLWave for mux testbench

---
+++ labs/02_mux/gtkwave.tcl
@@ -0,0 +1,17 @@
+# gtkwave::loadFile "dump.vcd"
+
+set all_signals [list]
+
+lappend all_signals tb.clk
+lappend all_signals tb.rst
+lappend all_signals tb.sel
+lappend all_signals tb.a
+lappend all_signals tb.b
+lappend all_signals tb.key
+lappend all_signals tb.led
+lappend all_signals {tb.led[0]}
+lappend all_signals tb.i_top.all_muxes
+
+set num_added [ gtkwave::addSignalsFromList $all_signals ]
+
+gtkwave::/Time/Zoom/Zoom_Full
