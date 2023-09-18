8b998f72b799f2284e6de778e31f2caa483abb85
Yan Shtunder <ya.shtunder@gmail.com>
2023-09-18 12:44:42 +0300

    Script support for Vivado configuration.
    New versions of Vivado use `open_hw_manager` to open the hardware tool.
    However, `open_hw_manager` is not supported in older versions. Therefore,
    will use `open_hw` to open the hardware tool. Older versions of Vivado will
    work correctly, but newer versions of Vivado will print a warning.

--- scripts/fpga/xilinx_vivado_configure.tcl
+++ scripts/fpga/xilinx_vivado_configure.tcl
@@ -3,13 +3,13 @@ if {! [info exists hw_device]} {
     error "The variable 'hw_device' is expected to be set"
 }
 
-open_hw_manager
+open_hw
 connect_hw_server
 open_hw_target
 set_property PROGRAM.FILE fpga_project.bit [get_hw_devices $hw_device]
 program_hw_devices                         [get_hw_devices $hw_device]
 close_hw_target
-close_hw_manager
+close_hw
 
 # See https://stackoverflow.com/questions/55495977/automate-the-usage-of-vivado-gui-by-using-tcl-scripts
 # See https://docs.xilinx.com/r/en-US/ug835-vivado-tcl-commands/program_hw_devices
