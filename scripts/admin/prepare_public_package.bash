b681f0896fc2e50d76ceb6fc9db5fbc4bbab6dbc
Yuri Panchul <yuri@panchul.com>
2023-09-20 20:14:42 -0700

    Improved error messages

--- scripts/admin/prepare_public_package.bash
+++ scripts/admin/prepare_public_package.bash
@@ -19,11 +19,11 @@ pkg_src_root_name=$(basename "$pkg_src_root")
 
 error ()
 {
-    // The "if" below is a protection against % in arguments
-    // which is interpreted as a format by printf.
-    //
-    // However this method has disadvantage:
-    // It prints "\n" for "\n" instead of a newline.
+    # The "if" below is a protection against % in arguments
+    # which is interpreted as a format by printf.
+    #
+    # However this method has disadvantage:
+    # It prints "\n" for "\n" instead of a newline.
 
     if [[ "$*" == *"%"* ]] ; then
         printf "\n$script: ERROR: %s\n" "$*" 1>&2
