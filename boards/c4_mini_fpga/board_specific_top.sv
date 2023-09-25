9fdd2fc39c405296f2eb21fb5a9e9bcedd7478f9
Alex Mikhailov <alexander.mikhailov@gmail.com>
2023-09-25 04:15:24 +0300

    Added baseline for Cyclone IV MINI FPGA board

--- boards/c4_mini_fpga/board_specific_top.sv
+++ boards/c4_mini_fpga/board_specific_top.sv
@@ -15,7 +15,7 @@ module board_specific_top
     input  [w_key     - 1:0] KEY_N,
     input  [w_sw      - 1:0] SW_N,
 
-    output [w_led     - 1:0] LED_N,
+    output logic [w_led     - 1:0] LED_N,
 
     output [            7:0] ABCDEFGH_N,
     output [w_digit   - 1:0] DIGIT_N,
@@ -23,19 +23,24 @@ module board_specific_top
     input                    UART_RX,
     output                   UART_TX,
 
-    inout  [w_gpio_j7 + 3 - 1:3] GPIO_J7,
+//    inout  [w_gpio_j7 + 3 - 1:3] GPIO_J7,
     inout  [w_gpio_p1 + 2 - 1:2] GPIO_P1,
     inout  [w_gpio_p2 + 2 - 1:2] GPIO_P2
 );
+/*
+wire rst = SW_N [7];
 
-    //------------------------------------------------------------------------
+logic [31:0] cnt;
 
-    wire [w_key   - 1:0] key;
-    wire [w_sw    - 1:0] sw;
-    wire [w_led   - 1:0] led;
+always @ (posedge CLK or posedge rst)
+    if (rst)
+        cnt <= '0;
+    else
+        cnt <= cnt + 1'd1;
 
-    wire [          7:0] abcdefgh;
-    wire [w_digit - 1:0] digit;
+assign LED_N = cnt [31:24];
+*/
+    localparam w_gpio = w_gpio_j7 + w_gpio_p1 + w_gpio_p2;
 
     //------------------------------------------------------------------------
 
@@ -43,7 +48,7 @@ module board_specific_top
     # (
         .clk_mhz ( clk_mhz ),
         .w_key   ( w_key   ),
-        .w_sw    ( w_sw    ),
+        .w_sw    ( w_sw - 1 ),
         .w_led   ( w_led   ),
         .w_digit ( w_digit ),
         .w_gpio  ( w_gpio  )
@@ -51,23 +56,17 @@ module board_specific_top
     i_top
     (
         .clk      ( CLK        ),
-        .rst      ( rst        ),
+        .rst      ( SW_N [7]   ),
 
-        .key      ( key        ),
-        .sw       ( key        ),
+        .key      ( KEY_N      ),
+        .sw       ( SW_N [6:0] ),
 
-        .led      ( LED        ),
+        .led      ( LED_N      ),
 
-        .abcdefgh ( abcdefgh   ),
-        .digit    ( digit      ),
+        .abcdefgh ( ABCDEFGH_N ),
+        .digit    ( DIGIT_N    ),
 
-        .gpio     ( GPIO       )
-
+        .gpio     (            )
     );
-
-    //------------------------------------------------------------------------
-
-    assign SEG_DATA  = ~ abcdefgh;
-    assign SEG_SEL   = ~ digit;
 
 endmodule
