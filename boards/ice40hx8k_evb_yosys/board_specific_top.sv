994daedebad2f7857188504a6d92baa8da3e20c3
Ruslan Zalata <rz@fabmicro.ru>
2023-09-27 00:53:34 +0000

    Add support for Yosys toolchain. Add board iCE40HX8K-EVB to test Yosys.
    The build itself system uses Makefile which is dynamically generated by
    these bash scripts. The following tools should be present in system to get
    things to work for iCE40 chips: make, yosys, nextpnr-ice40, icepack, icebram,
    iceprog, netlistsvg, xdot and firefox (optional).
    
    Although Yosys and Nextpnr are present in many Linux distros nowaways,
    it is still strongly recommended to install them from their corresponding
    repositories:
      https://github.com/YosysHQ/yosys.git - Yosys synthesis tool.
      https://github.com/YosysHQ/nextpnr - Place and route tools, depend on chip.
      https://github.com/YosysHQ/icestorm - Tools and database for Lattice iCE40.
      https://github.com/YosysHQ/prjtrellis - Tools and database for Lattice ECP5.
      https://github.com/YosysHQ/apicula - Tools and database for Gowin GW1N.
    
    All the gory details are on https://github.com/YosysHQ/
    
    When calling GUI mode, a set of SVG files will be generated:
      lab_name.svg - visualize netlist produced by Yosys during synthesis
      lab_name-placed.svg - visualize utilization of FPGA resources before routring
      lab_name-routed.svg - visualize FPGA resources and interconnects after routing
      lab_namd.doc - a GraphViz file for xdot utility, represents logical schematics
    
      Note1: some of these SVGs can be extremely large!
      Note2: 'top' module is used as root to elimination board specific stuff. So, if
      you want to see all the gori netlist you have to edit Makefile: change 'top' to
      'board_specific_top' and rebuid using 'make' or '03_synthesize_for_fpga.bash'.

---
+++ boards/ice40hx8k_evb_yosys/board_specific_top.sv
@@ -0,0 +1,182 @@
+   `define ENABLE_TM1638
+
+// 
+module board_specific_top
+# (
+    parameter   clk_mhz = 25, // CLK is derived from CLK100MHZ / 4 = 25MHz
+                w_key   = 3, // w_ket[2] is used for RST and is wired to JTAG_TMS.
+                w_sw    = 2,
+                w_led   = 2,
+                w_digit = 0,
+                w_gpio  = 12 
+)
+(
+    input                       CLK100MHZ,
+
+    input  [w_key       - 1:0]  KEY,
+    input  [w_sw        - 1:0]  SW,
+
+    input                       UART_RX,
+    output                      UART_TX,
+
+    output [w_led       - 1:0]  LED,
+
+    output                      VGA_HS,
+    output                      VGA_VS,
+    output [              3:0]  VGA_R,
+    output [              3:0]  VGA_G,
+    output [              3:0]  VGA_B,
+
+    inout  [w_gpio      - 1:0]  GPIO
+);
+
+    //------------------------------------------------------------------------
+
+    localparam w_tm_key    = 8,
+               w_tm_led    = 8,
+               w_tm_digit  = 8;
+
+
+    //------------------------------------------------------------------------
+
+    `ifdef ENABLE_TM1638    // TM1638 module is connected
+
+        localparam w_top_key   = w_tm_key,
+                   w_top_sw    = w_sw,
+                   w_top_led   = w_tm_led,
+                   w_top_digit = w_tm_digit;
+
+    `else                   // TM1638 module is not connected
+
+        localparam w_top_key   = w_key,
+                   w_top_sw    = w_sw,
+                   w_top_led   = w_led,
+                   w_top_digit = w_digit;
+
+    `endif
+
+    //------------------------------------------------------------------------
+
+    wire  [w_tm_key    - 1:0] tm_key;
+    wire  [w_tm_led    - 1:0] tm_led;
+    wire  [w_tm_digit  - 1:0] tm_digit;
+
+    logic [w_top_key   - 1:0] top_key;
+    wire  [w_top_led   - 1:0] top_led;
+    wire  [w_top_digit - 1:0] top_digit;
+
+    wire                      rst;
+    wire  [              7:0] abcdefgh;
+    wire  [             23:0] mic;
+
+    reg   [              1:0] clk_div;
+
+   //------------------------------------------------------------------------
+
+    `ifdef ENABLE_TM1638    // TM1638 module is connected
+
+        assign rst      = tm_key [w_tm_key - 1];
+        assign top_key  = tm_key [w_tm_key - 1:0];
+
+        assign tm_led   = top_led;
+        assign tm_digit = top_digit;
+
+    `else                   // TM1638 module is not connected
+
+        assign rst      = ~ KEY [w_key - 1];
+        assign top_key  = ~ KEY [w_key - 1:0];
+
+        assign LED      = ~ top_led;
+
+    `endif
+
+    //------------------------------------------------------------------------
+
+    top
+    # (
+        .clk_mhz ( clk_mhz   ),
+        .w_key   ( w_top_key ),  // The last key is used for a reset
+        .w_sw    ( w_top_sw      ),
+        .w_led   ( w_top_led     ),
+        .w_digit ( w_top_digit   ),
+        .w_gpio  ( w_gpio    )
+    )
+    i_top
+    (
+        .clk      ( CLK       ),
+        .rst      ( rst       ),
+
+        .key      ( top_key   ),
+        .sw       (           ),
+
+        .led      ( top_led   ),
+
+        .abcdefgh ( abcdefgh  ),
+        .digit    ( top_digit ),
+
+        .vsync    ( VGA_VS    ),
+        .hsync    ( VGA_HS    ),
+
+        .red      ( VGA_R     ),
+        .green    ( VGA_G     ),
+        .blue     ( VGA_B     ),
+
+        .mic      ( mic       ),
+        .gpio     (           )
+    );
+
+    //------------------------------------------------------------------------
+
+    wire [$left (abcdefgh):0] hgfedcba;
+
+    generate
+        genvar i;
+
+        for (i = 0; i < $bits (abcdefgh); i ++)
+        begin : abc
+            assign hgfedcba [i] = abcdefgh [$left (abcdefgh) - i];
+        end
+    endgenerate
+
+    //------------------------------------------------------------------------
+
+    tm1638_board_controller
+    # (
+        .w_digit ( w_tm_digit )
+    )
+    i_tm1638
+    (
+        .clk      ( CLK       ),
+        .rst      ( rst       ),
+        .hgfedcba ( hgfedcba  ),
+        .digit    ( tm_digit  ),
+        .ledr     ( tm_led    ),
+        .keys     ( tm_key    ),
+        .sio_clk  ( GPIO [0]  ),
+        .sio_stb  ( GPIO [1]  ),
+        .sio_data ( GPIO [2]  )
+    );
+
+    //------------------------------------------------------------------------
+
+    inmp441_mic_i2s_receiver i_microphone
+    (
+        .clk   ( clk      ),
+        .rst   ( rst      ),
+        .lr    ( GPIO [5] ),
+        .ws    ( GPIO [4] ),
+        .sck   ( GPIO [3] ),
+        .sd    ( GPIO [6] ),
+        .value ( mic      )
+    );
+
+    assign GPIO [8] = 1'b0;  // GND
+    assign GPIO [7] = 1'b1;  // VCC
+
+    always @ (posedge CLK100MHZ) begin //on each positive edge of 100Mhz clock increment clk_div
+        clk_div <= clk_div + 2'b1;
+    end
+
+    assign CLK = clk_div[1]; // CLK = CLK100MHZ / 4 = 25Mhz 
+
+endmodule
