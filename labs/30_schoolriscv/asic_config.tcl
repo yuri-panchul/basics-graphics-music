1278f4ad02c2761ec10e0715908cacf92daaae41
Yuri Panchul <yuri@panchul.com>
2024-02-25 22:58:44 -0800

    Fixed bug in synthesis script

--- labs/30_schoolriscv/asic_config.tcl
+++ labs/30_schoolriscv/asic_config.tcl
@@ -6,7 +6,7 @@ set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.*v]
 set ::env(CLOCK_PORT) "clk"
 
 set ::env(FP_SIZING) "absolute"
-set ::env(DIE_AREA) {0 0 300 300}
+set ::env(DIE_AREA) {0 0 200 200}
 
 set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
 
