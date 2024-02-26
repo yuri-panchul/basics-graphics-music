993773c8fa873413dbb98f942a06846d92af600b
Yuri Panchul <yuri@panchul.com>
2024-02-25 18:06:26 -0800

    Improving Open Lane support

--- labs/30_schoolriscv/asic_config.tcl
+++ labs/30_schoolriscv/asic_config.tcl
@@ -6,7 +6,7 @@ set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.*v]
 set ::env(CLOCK_PORT) "clk"
 
 set ::env(FP_SIZING) "absolute"
-set ::env(DIE_AREA) {0 0 100 100}
+set ::env(DIE_AREA) {0 0 1000 1000}
 
 set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
 
