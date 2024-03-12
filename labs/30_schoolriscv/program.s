33257d77f1992f91b162080ceb58e5dc2ab92f57
Yuri Panchul <yuri@panchul.com>
2024-03-12 04:29:52 -0700

    Removed tabs

--- labs/30_schoolriscv/program.s
+++ labs/30_schoolriscv/program.s
@@ -1,9 +1,9 @@
-# li pseudo-instruction 
+# li pseudo-instruction
 
-	li	t0, 0x123
-	li	t1, 0x12345678
-	li	t2, 0x12345000
-	li	t3, -0x123
+        li      t0, 0x123
+        li      t1, 0x12345678
+        li      t2, 0x12345000
+        li      t3, -0x123
 
 # RISC-V fibonacci program
 #
