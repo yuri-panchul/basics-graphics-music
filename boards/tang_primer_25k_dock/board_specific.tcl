9532436d0cb8300c7e32f176dea0a3495f77d66f
Yuri Panchul <yuri@panchul.com>
2024-05-11 02:12:32 -0700

    Tang Primer 25K started to work

--- boards/tang_primer_25k_dock/board_specific.tcl
+++ boards/tang_primer_25k_dock/board_specific.tcl
@@ -5,3 +5,8 @@ set_option -synthesis_tool gowinsynthesis
 set_option -output_base_name fpga_project
 set_option -top_module board_specific_top
 set_option -verilog_std sysv2017
+set_option -use_sspi_as_gpio 1
+set_option -use_cpu_as_gpio 1
+set_option -use_ready_as_gpio 1
+set_option -use_done_as_gpio 1
+# set_option -cmser_mode auto
