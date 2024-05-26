04436a807d4656aaa6bc9fd08b7f16e97e715061
Yuri Panchul <yuri@panchul.com>
2024-05-26 14:16:03 -0700

    Fixed runber support

--- boards/runber/board_specific.tcl
+++ boards/runber/board_specific.tcl
@@ -1,11 +1,12 @@
 # The synthesis options
 
-set_device GW1N-UV4LQ144C6/I5 -name GW1N-4
+set_device GW1N-UV4LQ144C6/I5 -name GW1N-4 -device_version B
 set_option -synthesis_tool gowinsynthesis
 set_option -output_base_name fpga_project
 set_option -top_module board_specific_top
 set_option -verilog_std sysv2017
 
+# set_option -use_jtag_as_gpio
 # set_option -use_sspi_as_gpio 1
 # set_option -cmser_mode auto
 
