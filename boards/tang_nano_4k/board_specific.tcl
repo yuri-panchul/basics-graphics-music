66e776579fb2eb3798f15c9c5d304f6c96cb6afe
Viktor Kurysh <viktorkurysh@gmail.com>
2024-06-02 12:10:23 -0700

    add tang_nano_4k board

---
+++ boards/tang_nano_4k/board_specific.tcl
@@ -0,0 +1,6 @@
+set_device GW1NSR-LV4CQN48PC6/I5 -device_version NA
+set_option -synthesis_tool gowinsynthesis
+set_option -output_base_name fpga_project
+set_option -top_module board_specific_top
+set_option -verilog_std sysv2017
+set_option -use_mspi_as_gpio 1
