95027098488812e02bba4f72893a49eaedb5f9fe
Yuri Panchul <yuri@panchul.com>
2024-07-22 18:24:30 -0700

    Unified lab headers everywhere

--- labs/2_graphics/2_2_game/lab_top.sv
+++ labs/2_graphics/2_2_game/lab_top.sv
@@ -2,16 +2,22 @@
 
 module lab_top
 # (
-    parameter clk_mhz   = 50,
-              pixel_mhz = 25,
-              w_key     = 4,
-              w_sw      = 8,
-              w_led     = 8,
-              w_digit   = 8,
-              w_gpio    = 100,
-              w_red     = 4,
-              w_green   = 4,
-              w_blue    = 4
+    parameter  clk_mhz       = 50,
+               w_key         = 4,
+               w_sw          = 8,
+               w_led         = 8,
+               w_digit       = 8,
+               w_gpio        = 100,
+
+               screen_width  = 640,
+               screen_height = 480,
+
+               w_red         = 4,
+               w_green       = 4,
+               w_blue        = 4,
+
+               w_x           = $clog2 ( screen_width  ),
+               w_y           = $clog2 ( screen_height )
 )
 (
     input                        clk,
@@ -29,44 +35,43 @@ module lab_top
     output logic [          7:0] abcdefgh,
     output logic [w_digit - 1:0] digit,
 
-    // VGA
+    // Graphics
 
-    output logic                 vsync,
-    output logic                 hsync,
+    input        [w_x     - 1:0] x,
+    input        [w_y     - 1:0] y,
+
     output logic [w_red   - 1:0] red,
     output logic [w_green - 1:0] green,
     output logic [w_blue  - 1:0] blue,
-    output                       display_on,
-    output                       pixel_clk,
 
-    input                        uart_rx,
-    output                       uart_tx,
+    // Microphone, sound output and UART
 
     input        [         23:0] mic,
     output       [         15:0] sound,
+
+    input                        uart_rx,
+    output                       uart_tx,
 
     // General-purpose Input/Output
 
     inout        [w_gpio  - 1:0] gpio
 );
 
-    localparam strobe_to_update_xy_counter_width
-        = $clog2 (clk_mhz * 1000 * 1000) - 6;
-
     //------------------------------------------------------------------------
 
        assign led        = '0;
        assign abcdefgh   = '0;
        assign digit      = '0;
-    // assign vsync      = '0;
-    // assign hsync      = '0;
     // assign red        = '0;
     // assign green      = '0;
     // assign blue       = '0;
-    // assign display_on = '0;
-    // assign pixel_clk  = '0;
        assign sound      = '0;
        assign uart_tx    = '1;
+
+    //------------------------------------------------------------------------
+
+    localparam strobe_to_update_xy_counter_width
+        = $clog2 (clk_mhz * 1000 * 1000) - 6;
 
     //------------------------------------------------------------------------
 
--- labs/2_graphics/2_3_crash_course_basics_and_graphics/lab_top.sv
+++ labs/2_graphics/2_3_crash_course_basics_and_graphics/lab_top.sv
@@ -2,17 +2,22 @@
 
 module lab_top
 # (
-    parameter clk_mhz   = 50,
-              pixel_mhz = 25,
-              w_key     = 4,
-              w_sw      = 8,
-              w_led     = 8,
-              w_digit   = 8,
-              w_gpio    = 100,
+    parameter  clk_mhz       = 50,
+               w_key         = 4,
+               w_sw          = 8,
+               w_led         = 8,
+               w_digit       = 8,
+               w_gpio        = 100,
 
-              w_red     = 4,
-              w_green   = 4,
-              w_blue    = 4
+               screen_width  = 640,
+               screen_height = 480,
+
+               w_red         = 4,
+               w_green       = 4,
+               w_blue        = 4,
+
+               w_x           = $clog2 ( screen_width  ),
+               w_y           = $clog2 ( screen_height )
 )
 (
     input                        clk,
@@ -30,21 +35,22 @@ module lab_top
     output logic [          7:0] abcdefgh,
     output logic [w_digit - 1:0] digit,
 
-    // VGA
+    // Graphics
 
-    output logic                 vsync,
-    output logic                 hsync,
+    input        [w_x     - 1:0] x,
+    input        [w_y     - 1:0] y,
+
     output logic [w_red   - 1:0] red,
     output logic [w_green - 1:0] green,
     output logic [w_blue  - 1:0] blue,
-    output                       display_on,
-    output                       pixel_clk,
 
-    input                        uart_rx,
-    output                       uart_tx,
+    // Microphone, sound output and UART
 
     input        [         23:0] mic,
     output       [         15:0] sound,
+
+    input                        uart_rx,
+    output                       uart_tx,
 
     // General-purpose Input/Output
 
@@ -56,13 +62,9 @@ module lab_top
     // assign led        = '0;
     // assign abcdefgh   = '0;
     // assign digit      = '0;
-    // assign vsync      = '0;
-    // assign hsync      = '0;
     // assign red        = '0;
     // assign green      = '0;
     // assign blue       = '0;
-    // assign display_on = '0;
-    // assign pixel_clk  = '0;
        assign sound      = '0;
        assign uart_tx    = '1;
 
