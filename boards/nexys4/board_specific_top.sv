30e8b9ab592b5bba6972811fa7587a7148c8c4d7
24x7fpga <kiran.24x7fpga@gmail.com>
2024-08-01 23:42:04 -0400

    updated board_specific_top for nexys4

--- boards/nexys4/board_specific_top.sv
+++ boards/nexys4/board_specific_top.sv
@@ -3,58 +3,68 @@
 
 module board_specific_top
 # (
-    parameter clk_mhz = 100,
-              w_key   = 5,
-              w_sw    = 16,
-              w_led   = 16,
-              w_digit = 8,
-              w_gpio  = 32
+    parameter clk_mhz       = 100,
+              w_key         = 5,
+              w_sw          = 16,
+              w_led         = 16,
+              w_digit       = 8,
+              w_gpio        = 32,
+              
+              screen_width  = 640,
+              screen_height = 480,
+
+              w_red         = 4,
+              w_green       = 4,
+              w_blue        = 4,
+
+              w_x           = $clog2 ( screen_width  ),
+              w_y           = $clog2 ( screen_height )
 )
 (
-    input         clk,
-    input         btnCpuReset,
+    input                   clk,
+    input                   btnCpuReset,
+  
+    input                   btnC,
+    input                   btnU,
+    input                   btnL,
+    input                   btnR,
+    input                   btnD,
 
-    input         btnC,
-    input         btnU,
-    input         btnL,
-    input         btnR,
-    input         btnD,
+    input  [w_sw      -1:0] sw,
+    output [w_led     -1:0] led,
 
-    input  [15:0] sw,
-    output [15:0] led,
+    output                  RGB1_Red,
+    output                  RGB1_Green,
+    output                  RGB1_Blue,
+  
+    output                  RGB2_Red,
+    output                  RGB2_Green,
+    output                  RGB2_Blue,
 
-    output        RGB1_Red,
-    output        RGB1_Green,
-    output        RGB1_Blue,
+    output [6           :0] seg,
+    output                  dp,
+    output [7           :0] an,
 
-    output        RGB2_Red,
-    output        RGB2_Green,
-    output        RGB2_Blue,
+    output                  Hsync,
+    output                  Vsync,
 
-    output [ 6:0] seg,
-    output        dp,
-    output [ 7:0] an,
+    output [w_red     -1:0] vgaRed,
+    output [w_blue    -1:0] vgaBlue,
+    output [w_green   -1:0] vgaGreen,
 
-    output        Hsync,
-    output        Vsync,
+    input                   RsRx,
 
-    output [ 3:0] vgaRed,
-    output [ 3:0] vgaBlue,
-    output [ 3:0] vgaGreen,
+    inout  [7           :0] JA,
+    inout  [7           :0] JB,
+    inout  [7           :0] JC,
+    inout  [7           :0] JD,
 
-    input         RsRx,
+    output                  micClk,
+    input                   micData,
+    output                  micLRSel,
 
-    inout  [ 7:0] JA,
-    inout  [ 7:0] JB,
-    inout  [ 7:0] JC,
-    inout  [ 7:0] JD,
-
-    output        micClk,
-    input         micData,
-    output        micLRSel,
-
-    output        ampPWM,
-    output        ampSD
+    output                  ampPWM,
+    output                  ampSD
 );
 
     //------------------------------------------------------------------------
@@ -92,6 +102,7 @@ module board_specific_top
     // FIXME: Should be assigned to some GPIO!
     wire        UART_TX;
     wire        UART_RX = '1;
+    wire [w_gpio      - 1:0]  gpio;
 
     //------------------------------------------------------------------------
 
@@ -104,40 +115,44 @@ module board_specific_top
 
     lab_top
     # (
-        .clk_mhz ( clk_mhz ),
-        .w_key   ( w_key   ),
-        .w_sw    ( w_sw    ),
-        .w_led   ( w_led   ),
-        .w_digit ( w_digit ),
-        .w_gpio  ( w_gpio  )
+        .clk_mhz       ( clk_mhz          ),
+        .w_key         ( w_key            ),
+        .w_sw          ( w_sw             ),
+        .w_led         ( w_led            ),
+        .w_digit       ( w_digit          ),
+        .w_gpio        ( w_gpio           ),
+        
+        .screen_width  (   screen_width   ),
+        .screen_height (   screen_height  ),
+
+        .w_red         (   w_red          ),
+        .w_green       (   w_green        ),
+        .w_blue        (   w_blue         )
     )
     i_lab_top
     (
-        .clk      ( clk         ),
-        .slow_clk ( slow_clk    ),
-        .rst      ( rst         ),
+        .clk           ( clk              ),
+        .slow_clk      ( slow_clk         ),
+        .rst           ( rst              ),
 
-        .key      ( { btnD, btnU, btnL, btnC, btnR } ),
-        .sw       ( sw          ),
+        .key           ( { btnD, btnU, btnL, btnC, btnR } ),
+        .sw            ( sw               ),
 
-        .led      ( led         ),
+        .led           ( led              ),
 
-        .abcdefgh ( abcdefgh    ),
+        .abcdefgh      ( abcdefgh         ),
 
-        .digit    ( digit       ),
+        .digit         ( digit            ),
 
-        .vsync    ( Vsync       ),
-        .hsync    ( Hsync       ),
+        .red           ( vgaRed           ),
+        .green         ( vgaBlue          ),
+        .blue          ( vgaGreen         ),
 
-        .red      ( vgaRed      ),
-        .green    ( vgaBlue     ),
-        .blue     ( vgaGreen    ),
+        .uart_rx       ( UART_RX          ),
+        .uart_tx       ( UART_TX          ),
 
-        .uart_rx  ( UART_RX     ),
-        .uart_tx  ( UART_TX     ),
-
-        .mic      ( mic         ),
-        .gpio     (             )
+        .mic           ( mic              ),
+        .gpio          ( gpio             )
     );
 
     //------------------------------------------------------------------------
