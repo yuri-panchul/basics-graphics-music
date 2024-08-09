14c76f8e6cd0cee6c48b6a84f245b900aaa5528f
Yuri Panchul <yuri@panchul.com>
2024-08-09 16:04:18 -0700

    Tang mega 138k pro dock - no tm1638 version

--- boards/tang_mega_pro_138k_lcd_480_272_no_tm1638/board_specific.tcl
+++ boards/tang_mega_pro_138k_lcd_480_272_no_tm1638/board_specific.tcl
@@ -1,11 +1,7 @@
 # Synthesis and Place & Route settings
 
-set_device GW1NR-LV9QN88PC6/I5 -name GW1NR-9 -device_version C
-
+set_device GW5AST-LV138FPG676AES -name GW5AST-138 -device_version B
 set_option -synthesis_tool gowinsynthesis
 set_option -output_base_name fpga_project
 set_option -top_module board_specific_top
 set_option -verilog_std sysv2017
-
-set_option -use_mspi_as_gpio 1
-set_option -use_sspi_as_gpio 1
