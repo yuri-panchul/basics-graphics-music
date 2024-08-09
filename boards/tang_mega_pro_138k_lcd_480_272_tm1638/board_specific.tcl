ab7024d3bdf9f1724acefa3d7b9af483d9405559
Yuri Panchul <yuri@panchul.com>
2024-08-09 15:05:47 -0700

    Baseline wrapper files for tang mega 138k pro dock

---
+++ boards/tang_mega_pro_138k_lcd_480_272_tm1638/board_specific.tcl
@@ -0,0 +1,7 @@
+# Synthesis and Place & Route settings
+
+set_device GW5AST-LV138FPG676AES -name GW5AST-138 -device_version B
+set_option -synthesis_tool gowinsynthesis
+set_option -output_base_name fpga_project
+set_option -top_module board_specific_top
+set_option -verilog_std sysv2017
