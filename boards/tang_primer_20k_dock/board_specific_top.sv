d852575c9fe6c91a23fcca34e9fad2b930e3685f
mkudinov <m.kudinov@icloud.com>
2024-08-29 23:29:53 +0300

    Update with new graphics structure

--- boards/tang_primer_20k_dock/board_specific_top.sv
+++ boards/tang_primer_20k_dock/board_specific_top.sv
@@ -1,14 +1,34 @@
 `include "config.svh"
 `include "lab_specific_board_config.svh"
 
+`ifdef FORCE_NO_INSTANTIATE_TM1638_BOARD_CONTROLLER_MODULE
+    `undef INSTANTIATE_TM1638_BOARD_CONTROLLER_MODULE
+`endif
+
+`ifdef FORCE_NO_INSTANTIATE_GRAPHICS_INTERFACE_MODULE
+   `undef INSTANTIATE_GRAPHICS_INTERFACE_MODULE
+`endif
+
 module board_specific_top
 # (
-    parameter   clk_mhz = 27,
-                w_key   = 5,  // The last key is used for a reset
-                w_sw    = 5,
-                w_led   = 6,
-                w_digit = 0,
-                w_gpio  = 32
+    parameter   clk_mhz       = 27,
+                pixel_mhz     = 25,
+
+                w_key         = 5,  // The last key is used for a reset
+                w_sw          = 5,
+                w_led         = 6,
+                w_digit       = 0,
+                w_gpio        = 32,
+
+                screen_width  = 640,
+                screen_height = 480,
+
+                w_red         = 8,
+                w_green       = 8,
+                w_blue        = 8,
+
+                w_x = $clog2 ( screen_width ),
+                w_y = $clog2 ( screen_height )
 )
 (
     input                       CLK,
@@ -24,10 +44,13 @@ module board_specific_top
     inout  [w_gpio / 4  - 1:0]  GPIO_0,
     inout  [w_gpio / 4  - 1:0]  GPIO_1,
     inout  [w_gpio / 4  - 1:0]  GPIO_2,
-    inout  [w_gpio / 4  - 1:0]  GPIO_3
-);
+    inout  [w_gpio / 4  - 1:0]  GPIO_3,
 
-    wire clk = CLK;
+    output                      TMDS_CLK_N,
+    output                      TMDS_CLK_P,
+    output [              2:0]  TMDS_D_N,
+    output [              2:0]  TMDS_D_P
+);
 
     //------------------------------------------------------------------------
 
@@ -64,22 +87,41 @@ module board_specific_top
     wire  [w_lab_led   - 1:0] lab_led;
     wire  [w_lab_digit - 1:0] lab_digit;
 
+    wire                      clk;
     wire                      rst;
     wire  [              7:0] abcdefgh;
+
+    wire  [w_x         - 1:0] x;
+    wire  [w_y         - 1:0] y;
+
+    wire  [w_red       - 1:0] red;
+    wire  [w_green     - 1:0] green;
+    wire  [w_blue      - 1:0] blue;
+
     wire  [             23:0] mic;
-
-    wire                      VGA_HS;
-    wire                      VGA_VS;
-
-    wire  [              3:0] VGA_R;
-    wire  [              3:0] VGA_G;
-    wire  [              3:0] VGA_B;
 
     //------------------------------------------------------------------------
 
+    `ifdef INSTANTIATE_GRAPHICS_INTERFACE_MODULE
+
+        localparam lab_mhz = pixel_mhz;
+        assign     clk     = pixel_clk;
+
+    `else
+
+        localparam lab_mhz = clk_mhz;
+        assign     clk     = CLK;
+
+    `endif
+
+    //------------------------------------------------------------------------
+
+    // Always use button on board for reset, otherwise the board would be
+    // stuck on reset if tm1638 is not connected
+    assign rst = ~ KEY [w_key - 1];
+
     `ifdef INSTANTIATE_TM1638_BOARD_CONTROLLER_MODULE
 
-        assign rst      = tm_key [w_tm_key - 1];
         assign lab_key  = tm_key [w_tm_key - 1:0];
         assign lab_sw   = ~ SW;
 
@@ -88,7 +130,6 @@ module board_specific_top
 
     `else                   // TM1638 module is not connected
 
-        assign rst      = ~ KEY [w_key - 1];
         assign lab_key  = ~ KEY [w_key - 1:0];
         assign lab_sw   = ~ SW;
 
@@ -107,42 +148,52 @@ module board_specific_top
 
     lab_top
     # (
-        .clk_mhz ( clk_mhz      ),
-        .w_key   ( w_lab_key    ),  // The last key is used for a reset
-        .w_sw    ( w_lab_sw     ),
-        .w_led   ( w_lab_led    ),
-        .w_digit ( w_lab_digit  ),
-        .w_gpio  ( w_gpio       )
+        .clk_mhz       ( lab_mhz       ),
+
+        .w_key         ( w_lab_key     ),  // The last key is used for a reset
+        .w_sw          ( w_lab_key     ),
+        .w_led         ( w_lab_led     ),
+        .w_digit       ( w_lab_digit   ),
+        .w_gpio        ( w_gpio        ),
+
+        .screen_width  ( screen_width  ),
+        .screen_height ( screen_height ),
+
+        .w_red         ( w_red         ),
+        .w_green       ( w_green       ),
+        .w_blue        ( w_blue        )
     )
     i_lab_top
     (
-        .clk      ( clk       ),
-        .slow_clk ( slow_clk  ),
-        .rst      ( rst       ),
+        .clk           ( clk           ),
+        .slow_clk      ( slow_clk      ),
+        .rst           ( rst           ),
 
-        .key      ( lab_key   ),
-        .sw       ( lab_sw    ),
+        .key           ( lab_key       ),
+        .sw            ( lab_sw        ),
 
-        .led      ( lab_led   ),
+        .led           ( lab_led       ),
 
-        .abcdefgh ( abcdefgh  ),
-        .digit    ( lab_digit ),
+        .abcdefgh      ( abcdefgh      ),
+        .digit         ( lab_digit     ),
 
-        .vsync    ( VGA_VS    ),
-        .hsync    ( VGA_HS    ),
+        .x             ( x             ),
+        .y             ( y             ),
 
-        .red      ( VGA_R     ),
-        .green    ( VGA_G     ),
-        .blue     ( VGA_B     ),
+        .red           ( red           ),
+        .green         ( green         ),
+        .blue          ( blue          ),
 
-        .uart_rx  ( UART_RX   ),
-        .uart_tx  ( UART_TX   ),
+        .uart_rx       ( UART_RX       ),
+        .uart_tx       ( UART_TX       ),
 
-        .mic      ( mic       ),
-        .gpio     (           )
+        .mic           ( mic           ),
+        .gpio          (               )
     );
 
     //------------------------------------------------------------------------
+
+    `ifdef INSTANTIATE_TM1638_BOARD_CONTROLLER_MODULE
 
     wire [$left (abcdefgh):0] hgfedcba;
 
@@ -159,7 +210,7 @@ module board_specific_top
 
     tm1638_board_controller
     # (
-        .clk_mhz ( clk_mhz ),
+        .clk_mhz ( lab_mhz ),
         .w_digit ( w_tm_digit )
     )
     i_tm1638
@@ -175,11 +226,78 @@ module board_specific_top
         .sio_data   ( GPIO_0[1]     )
     );
 
+    `endif
+
     //------------------------------------------------------------------------
+
+    `ifdef INSTANTIATE_GRAPHICS_INTERFACE_MODULE
+
+    localparam serial_clk_mhz = 125;
+
+    wire serial_clk;
+
+    Gowin_rPLL i_Gowin_rPLL
+    (
+        .clkin  ( CLK        ), // 27 MHz
+        .clkout ( serial_clk ), // 126 MHz
+        .lock   (            )
+    );
+
+    //--------------------------------------------------------------------
+
+    wire hsync, vsync, display_on, pixel_clk, raw_pixel_clk;
+
+    wire [9:0] x10; assign x = x10;
+    wire [9:0] y10; assign y = y10;
+
+    vga
+    # (
+        .CLK_MHZ     ( serial_clk_mhz  ),
+        .PIXEL_MHZ   ( pixel_mhz       )
+    )
+    i_vga
+    (
+        .clk         ( serial_clk      ),
+        .rst         ( rst             ),
+        .hsync       ( hsync           ),
+        .vsync       ( vsync           ),
+        .display_on  ( display_on      ),
+        .hpos        ( x10             ),
+        .vpos        ( y10             ),
+        .pixel_clk   ( raw_pixel_clk   )
+    );
+
+    // Works without BUFG, but seems like a good practice
+    BUFG i_BUFG (.I (raw_pixel_clk), .O (pixel_clk));
+
+    //--------------------------------------------------------------------
+
+    DVI_TX_Top i_DVI_TX_Top
+    (
+        .I_rst_n       ( ~ rst         ),
+        .I_serial_clk  (   serial_clk  ),
+        .I_rgb_clk     (   pixel_clk   ),
+        .I_rgb_vs      ( ~ vsync       ),
+        .I_rgb_hs      ( ~ hsync       ),
+        .I_rgb_de      (   display_on  ),
+        .I_rgb_r       (   red         ),
+        .I_rgb_g       (   green       ),
+        .I_rgb_b       (   blue        ),
+        .O_tmds_clk_p  (   TMDS_CLK_P  ),
+        .O_tmds_clk_n  (   TMDS_CLK_N  ),
+        .O_tmds_data_p (   TMDS_D_P    ),
+        .O_tmds_data_n (   TMDS_D_N    )
+    );
+
+    `endif
+
+    //--------------------------------------------------------------------
+
+    `ifdef INSTANTIATE_MICROPHONE_INTERFACE_MODULE
 
     inmp441_mic_i2s_receiver
     # (
-        .clk_mhz ( clk_mhz    )
+        .clk_mhz ( lab_mhz    )
     )
     i_microphone
     (
@@ -192,9 +310,6 @@ module board_specific_top
         .value   ( mic        )
     );
 
-    //------------------------------------------------------------------------
-
-    assign GPIO_3 = { VGA_B, VGA_R };
-    assign GPIO_2 = { VGA_HS, VGA_VS, 2'bz, VGA_G };
+    `endif
 
 endmodule
