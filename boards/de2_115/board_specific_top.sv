b1aeee8547405610e4ed2ac32be6e9a2b2449e16
Alexander Kirichenko <aakirichenko@google.com>
2024-09-13 09:16:14 +0500

    de2_115: vga dead code was removed, comment update

--- boards/de2_115/board_specific_top.sv
+++ boards/de2_115/board_specific_top.sv
@@ -70,7 +70,7 @@ module board_specific_top
     wire                    clk     = CLOCK_50;
     wire                    rst     = SW [w_lab_sw];
 
-    // Keys, switches, LEDs
+    // Switches, LEDs
 
     wire [ w_lab_sw  - 1:0] lab_sw  = SW [w_lab_sw - 1:0];
     wire [ w_led     - 1:0] lab_led;
@@ -85,11 +85,7 @@ module board_specific_top
     wire [ w_x       - 1:0] x;
     wire [ w_y       - 1:0] y;
 
-    wire [ w_red     - 1:0] red;
-    wire [ w_green   - 1:0] green;
-    wire [ w_blue    - 1:0] blue;
-
-    // Microphone, sound output and UART
+    // Microphone, sound output
 
     wire [            23:0] mic;
     wire [            15:0] sound;
