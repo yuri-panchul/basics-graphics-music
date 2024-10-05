8276c2509adc24c839b0db86bbb027312eb942e8
Yuri Panchul <yuri@panchul.com>
2024-10-04 23:16:52 -0700

    More changes for Tang 25k PMOD DVI configuration

--- boards/tang_primer_25k_pmod_vga/board_specific_top.sv
+++ boards/tang_primer_25k_pmod_vga/board_specific_top.sv
@@ -203,29 +203,70 @@ module board_specific_top
 
     `ifdef INSTANTIATE_GRAPHICS_INTERFACE_MODULE
 
-        wire vsync, hsync;
+        wire vga_in_clk;
+
+        `ifdef USE_PMOD_DVI
+
+            localparam vga_in_clk_mhz = 125;
+
+            Gowin_PLL i_gowin_pll
+            (
+                .lock    (            ),
+                .clkout0 ( vga_in_clk ),
+                .clkin   ( clk        )
+            );
+
+        `else  // Instead of DVI use VGA
+
+            localparam vga_in_clk_mhz = clk_mhz;
+            assign     vga_in_clk     = clk;
+
+        `endif
+
+        //--------------------------------------------------------------------
+
+        wire hsync, vsync, display_on, pixel_clk;
 
         wire [9:0] x10; assign x = x10;
         wire [9:0] y10; assign y = y10;
 
         vga
         # (
-            .CLK_MHZ     ( clk_mhz    ),
-            .PIXEL_MHZ   ( pixel_mhz  )
+            .CLK_MHZ     ( vga_in_clk_mhz ),
+            .PIXEL_MHZ   ( pixel_mhz      )
         )
         i_vga
         (
-            .clk         ( clk        ),
-            .rst         ( rst        ),
-            .hsync       ( hsync      ),
-            .vsync       ( vsync      ),
-            .display_on  ( display_on ),
-            .hpos        ( x10        ),
-            .vpos        ( y10        ),
-            .pixel_clk   (            )
+            .clk         ( vga_in_clk     ),
+            .rst         ( rst            ),
+            .hsync       ( hsync          ),
+            .vsync       ( vsync          ),
+            .display_on  ( display_on     ),
+            .hpos        ( x10            ),
+            .vpos        ( y10            ),
+            .pixel_clk   ( pixel_clk      )
         );
 
+        //--------------------------------------------------------------------
+
         `ifdef USE_PMOD_DVI
+
+            DVI_TX_Top i_DVI_TX_Top
+            (
+                .I_rst_n       ( ~ rst         ),
+                .I_serial_clk  (   vga_in_clk  ),
+                .I_rgb_clk     (   pixel_clk   ),
+                .I_rgb_vs      ( ~ vsync       ),
+                .I_rgb_hs      ( ~ hsync       ),
+                .I_rgb_de      (   display_on  ),
+                .I_rgb_r       (   red         ),
+                .I_rgb_g       (   green       ),
+                .I_rgb_b       (   blue        ),
+                .O_tmds_clk_p  (   tmds_clk_p  ),
+                .O_tmds_clk_n  (   tmds_clk_n  ),
+                .O_tmds_data_p (   tmds_d_p    ),
+                .O_tmds_data_n (   tmds_d_n    )
+            );
 
         `else  // Instead of DVI use VGA
 
