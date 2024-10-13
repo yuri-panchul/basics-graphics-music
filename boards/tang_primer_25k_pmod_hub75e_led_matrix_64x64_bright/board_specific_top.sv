f90efff8f0b5f53d11147472a440da2798336081
Yuri Panchul <yuri@panchul.com>
2024-10-12 22:39:13 -0700

    Added led matrix brightness

--- boards/tang_primer_25k_pmod_hub75e_led_matrix_64x64_bright/board_specific_top.sv
+++ boards/tang_primer_25k_pmod_hub75e_led_matrix_64x64_bright/board_specific_top.sv
@@ -1,2 +1,2 @@
 `define HUB75E_LED_MATRIX_BRIGHTNESS 8
-`include "../tang_primer_25k_pmod_hub75e_led_matrix_64x64_bright/board_specific_top.sv"
+`include "../tang_primer_25k_pmod_hub75e_led_matrix_64x64/board_specific_top.sv"
