8c898931cbb19ce580404a039ba2c2daf11c7e34
Aleksandr Ryabov <a.ryabov@innopolis.university>
2024-10-13 15:20:21 +0300

    Update immp441 reseiver

--- peripherals/inmp441_mic_i2s_receiver.sv
+++ peripherals/inmp441_mic_i2s_receiver.sv
@@ -20,21 +20,49 @@ module inmp441_mic_i2s_receiver
 
     //------------------------------------------------------------------------
 
+    parameter int BASE_CLK_MHZ = 50;
+
     logic clk_en;
 
     generate
-        if (clk_mhz == 100)
-        begin : clk_100
+        if (clk_mhz > BASE_CLK_MHZ) 
+        begin : g_cnt_enable
+            
+            localparam int clk_ratio = (clk_mhz * 1000 * 1000) / (BASE_CLK_MHZ * 1000 * 1000);
+            localparam int w_cnt = $clog2 (clk_ratio);
+
+            logic [w_cnt - 1:0] cnt;
+
             always_ff @ (posedge clk or posedge rst)
+            begin
                 if (rst)
-                    clk_en <= '0;
+                begin
+                    cnt     <= '0;
+                    clk_en  <= '0;
+                end
+                else if (cnt == '0)
+                begin
+                    cnt     <= w_cnt' (clk_ratio - 1);
+                    clk_en  <= '1;
+                end
                 else
-                    clk_en <= ~ clk_en;
+                begin
+                    cnt     <= cnt - 1'd1;
+                    clk_en  <= '0;
+                end
+            end
+
         end
-        else
-        begin : not_clk_100
+        else if (clk_mhz == BASE_CLK_MHZ)
+        begin : g_full_enable
             assign clk_en = '1;
         end
+        else
+        begin : g_enable_is_not_possible
+            $fatal("Generation of clk_enable is not possible for given frequency. Input frequency must by greater then 50 MHz.");
+            assign clk_en = '0;
+        end
+
     endgenerate
 
     //------------------------------------------------------------------------
