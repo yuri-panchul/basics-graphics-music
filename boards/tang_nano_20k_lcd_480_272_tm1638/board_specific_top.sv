574e6b7438d00006f25ba1212b86a4a11736bd51
Yuri Panchul <yuri@panchul.com>
2024-11-03 20:04:01 -0800

    Edited Timothy's checkin

--- boards/tang_nano_20k_lcd_480_272_tm1638/board_specific_top.sv
+++ boards/tang_nano_20k_lcd_480_272_tm1638/board_specific_top.sv
@@ -14,87 +14,6 @@
 
 //----------------------------------------------------------------------------
 
-module lcd_timing
-(
-    input                   PixelClk,
-    input                   nRST,
-
-    output                  LCD_DE,
-
-	output          [4:0]   LCD_B,
-	output          [5:0]   LCD_G,
-	output          [4:0]   LCD_R
-);
-	
-    // Horizen count to Hsync, then next Horizen line.
-
-    parameter       H_Pixel_Valid    = 16'd480; 
-    parameter       H_FrontPorch     = 16'd50;
-    parameter       H_BackPorch      = 16'd30;  
-
-    parameter       PixelForHS       = H_Pixel_Valid + H_FrontPorch + H_BackPorch;
-
-    parameter       V_Pixel_Valid    = 16'd272; 
-    parameter       V_FrontPorch     = 16'd20;  
-    parameter       V_BackPorch      = 16'd5;    
-
-    parameter       PixelForVS       = V_Pixel_Valid + V_FrontPorch + V_BackPorch;
-
-    // Horizen pixel count
-
-    reg         [15:0]  H_PixelCount;
-    reg         [15:0]  V_PixelCount;
-
-    always @(  posedge PixelClk or negedge nRST  )begin
-        if( !nRST ) begin
-            V_PixelCount      <=  16'b0;    
-            H_PixelCount      <=  16'b0;
-            end
-        else if(  H_PixelCount == PixelForHS ) begin
-            V_PixelCount      <=  V_PixelCount + 1'b1;
-            H_PixelCount      <=  16'b0;
-            end
-        else if(  V_PixelCount == PixelForVS ) begin
-            V_PixelCount      <=  16'b0;
-            H_PixelCount      <=  16'b0;
-            end
-        else begin
-            V_PixelCount      <=  V_PixelCount ;
-            H_PixelCount      <=  H_PixelCount + 1'b1;
-        end
-    end
-
-    // SYNC-DE MODE
-
-    assign  LCD_DE =    ( H_PixelCount >= H_BackPorch ) && ( H_PixelCount <= H_Pixel_Valid + H_BackPorch ) &&
-                        ( V_PixelCount >= V_BackPorch ) && ( V_PixelCount <= V_Pixel_Valid + V_BackPorch ) && PixelClk;
-
-    // color bar
-    localparam          Colorbar_width   =   H_Pixel_Valid / 16;
-
-    assign  LCD_R     = ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 0  )) ? 5'b00000 :
-                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 1  )) ? 5'b00001 : 
-                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 2  )) ? 5'b00010 :    
-                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 3  )) ? 5'b00100 :    
-                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 4  )) ? 5'b01000 :    
-                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 5  )) ? 5'b10000 :  5'b00000;
-
-    assign  LCD_G    =  ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 6  )) ? 6'b000001: 
-                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 7  )) ? 6'b000010:    
-                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 8  )) ? 6'b000100:    
-                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 9  )) ? 6'b001000:    
-                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 10 )) ? 6'b010000:    
-                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 11 )) ? 6'b100000:  6'b000000;
-
-    assign  LCD_B    =  ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 12 )) ? 5'b00001 : 
-                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 13 )) ? 5'b00010 :    
-                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 14 )) ? 5'b00100 :    
-                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 15 )) ? 5'b01000 :    
-                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 16 )) ? 5'b10000 :  5'b00000;
-
-endmodule
-
-
 module board_specific_top
 # (
     parameter clk_mhz       = 27,
@@ -130,7 +49,7 @@ module board_specific_top
 
     input  [w_key        - 1:0]  KEY,
 
-    //output [w_led        - 1:0]  LED,
+    output [w_led        - 1:0]  LED,
 
     // Some LCD pins share bank with TMDS pins
     // which have different voltage requirements.
@@ -139,26 +58,28 @@ module board_specific_top
     // because they are assigned to a different bank.
 
     output                       LCD_DEN,
-    //output                       LCD_VS,
-    //output                       LCD_HS,
+    output                       LCD_VS,
+    output                       LCD_HS,
     output                       LCD_CLK,
 
     output [7:7 + 1 - w_red   ]  LCD_R,
     output [7:7 + 1 - w_green ]  LCD_G,
     output [7:7 + 1 - w_blue  ]  LCD_B
-    //input                        UART_RX,
-    //output                       UART_TX,
 
-    //inout  [w_gpio       - 1:0]  GPIO,
+    input                        UART_RX,
+    output                       UART_TX,
+
+    inout  [w_gpio       - 1:0]  GPIO,
 
     // DVI ports
+
     // output                    O_TMDS_CLK_N,
     // output                    O_TMDS_CLK_P,
     // output [            2:0]  O_TMDS_DATA_N,
     // output [            2:0]  O_TMDS_DATA_P,
 
-    //inout                        EDID_CLK,
-    //inout                        EDID_DAT,
+    inout                        EDID_CLK,
+    inout                        EDID_DAT,
 
     // output                    JOYSTICK_CLK,
     // output                    JOYSTICK_MOSI,
@@ -167,46 +88,26 @@ module board_specific_top
 
     // output                    JOYSTICK_CLK2,
     // output                    JOYSTICK_MOSI2,
-    //output                       JOYSTICK_MISO2,
-    //output                       JOYSTICK_CS2,
+    output                       JOYSTICK_MISO2,
+    output                       JOYSTICK_CS2,
 
     // SD card ports
-    //output                       SD_CLK,
-    //output                       SD_CMD,
-    //inout                        SD_DAT0,
-    //inout                        SD_DAT1,
-    //inout                        SD_DAT2,
-    //inout                        SD_DAT3,
+    output                       SD_CLK,
+    output                       SD_CMD,
+    inout                        SD_DAT0,
+    inout                        SD_DAT1,
+    inout                        SD_DAT2,
+    inout                        SD_DAT3,
 
     // Ports for on-board I2S amplifier
-    //output                       HP_BCK,
-    //output                       HP_DIN,
-    //output                       HP_WS,
-    //output                       PA_EN,
+    output                       HP_BCK,
+    output                       HP_DIN,
+    output                       HP_WS,
+    output                       PA_EN,
 
     // On-board WS2812 RGB LED with a serial interface
-    //inout                        WS2812
+    inout                        WS2812
 );
-
-
-    Gowin_rPLL Gowin_rPLL_9Mhz(
-        .clkout(LCD_CLK), // 9MHz
-        .clkin(CLK)   //27MHz
-    );
-
-	lcd_timing	lcd_timing_inst(
-		.PixelClk	(	LCD_CLK		 ),
-		.nRST		(	~ KEY [0]      ),
-
-		.LCD_DE		(	LCD_DEN	 	 ),
-
-		.LCD_B		(	LCD_B		 ),
-		.LCD_G		(	LCD_G		 ),
-		.LCD_R		(	LCD_R		 )
-	);
-
-
-`ifdef UNDEFINED
 
     wire clk = CLK;
 
@@ -412,7 +313,7 @@ module board_specific_top
         Gowin_rPLL i_Gowin_rPLL
         (
             .clkout  ( lcd_module_clk ),  // 200    MHz
-            .clkoutd ( LCD_CLK   ),        //  33.33 MHz
+            .clkoutd ( LCD_CLK        ),  //  33.33 MHz
             .clkin   ( clk            )   //  27    MHz
         );
 
@@ -420,8 +321,8 @@ module board_specific_top
 
             Gowin_rPLL Gowin_rPLL
             (
-                .clkout  ( LCD_CLK   ),  //  9 MHz
-                .clkin   ( clk            )   // 27 MHz
+                .clkout ( LCD_CLK ),  //  9 MHz
+                .clkin  ( clk     )   // 27 MHz
             );
 
         `endif
@@ -437,19 +338,22 @@ module board_specific_top
             .CLK       (   lcd_module_clk ),
             `endif
 
-            .PixelClk  (   LCD_CLK   ),
+            .PixelClk  (   LCD_CLK        ),
             .nRST      ( ~ rst            ),
 
-            .LCD_DE   (   LCD_DEN  ),
-            //.LCD_HSYNC (   LCD_HS  ),
-            //.LCD_VSYNC (   LCD_VS  ),
+            .LCD_DE    (   LCD_DEN        ),
+            .LCD_HSYNC (                  ),
+            .LCD_VSYNC (                  ),
 
-            .x         (   x       ),
-            .y         (   y       )
+            .x         (   x              ),
+            .y         (   y              )
         );
 
-        assign LCD_INIT = 1'b0;
-        assign LCD_BL   = 1'b0;
+        assign LCD_HSYNC = 1'b0;
+        assign LCD_VSYNC = 1'b0;
+
+        assign LCD_INIT  = 1'b0;
+        assign LCD_BL    = 1'b0;
 
     `endif
 
@@ -500,7 +404,5 @@ module board_specific_top
         assign PA_EN = 1'b1;
 
     `endif
-
-`endif
 
 endmodule
