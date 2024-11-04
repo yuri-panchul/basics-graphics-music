1c5567951a096b82bf8084a0785fa0b7a4006685
Yuri Panchul <yuri@panchul.com>
2024-11-03 22:22:18 -0800

    Fixed LCD bugs

--- boards/tang_nano_20k_lcd_480_272_tm1638/board_specific_top.sv
+++ boards/tang_nano_20k_lcd_480_272_tm1638/board_specific_top.sv
@@ -4,6 +4,8 @@
 
 //----------------------------------------------------------------------------
 
+`define FORCE_NO_INSTANTIATE_TM1638_BOARD_CONTROLLER_MODULE
+
 `ifdef FORCE_NO_INSTANTIATE_TM1638_BOARD_CONTROLLER_MODULE
     `undef INSTANTIATE_TM1638_BOARD_CONTROLLER_MODULE
 `endif
@@ -13,6 +15,87 @@
 `define REVERSE_LED
 
 //----------------------------------------------------------------------------
+
+module lcd_timing
+(
+    input                   PixelClk,
+    input                   nRST,
+
+    output                  LCD_DE,
+
+	output          [4:0]   LCD_B,
+	output          [5:0]   LCD_G,
+	output          [4:0]   LCD_R
+);
+	
+    // Horizen count to Hsync, then next Horizen line.
+
+    parameter       H_Pixel_Valid    = 16'd480; 
+    parameter       H_FrontPorch     = 16'd50;
+    parameter       H_BackPorch      = 16'd30;  
+
+    parameter       PixelForHS       = H_Pixel_Valid + H_FrontPorch + H_BackPorch;
+
+    parameter       V_Pixel_Valid    = 16'd272; 
+    parameter       V_FrontPorch     = 16'd20;  
+    parameter       V_BackPorch      = 16'd5;    
+
+    parameter       PixelForVS       = V_Pixel_Valid + V_FrontPorch + V_BackPorch;
+
+    // Horizen pixel count
+
+    reg         [15:0]  H_PixelCount;
+    reg         [15:0]  V_PixelCount;
+
+    always @(  posedge PixelClk or negedge nRST  )begin
+        if( !nRST ) begin
+            V_PixelCount      <=  16'b0;    
+            H_PixelCount      <=  16'b0;
+            end
+        else if(  H_PixelCount == PixelForHS ) begin
+            V_PixelCount      <=  V_PixelCount + 1'b1;
+            H_PixelCount      <=  16'b0;
+            end
+        else if(  V_PixelCount == PixelForVS ) begin
+            V_PixelCount      <=  16'b0;
+            H_PixelCount      <=  16'b0;
+            end
+        else begin
+            V_PixelCount      <=  V_PixelCount ;
+            H_PixelCount      <=  H_PixelCount + 1'b1;
+        end
+    end
+
+    // SYNC-DE MODE
+
+    assign  LCD_DE =    ( H_PixelCount >= H_BackPorch ) && ( H_PixelCount <= H_Pixel_Valid + H_BackPorch ) &&
+                        ( V_PixelCount >= V_BackPorch ) && ( V_PixelCount <= V_Pixel_Valid + V_BackPorch ) && PixelClk;
+
+    // color bar
+    localparam          Colorbar_width   =   H_Pixel_Valid / 16;
+
+    assign  LCD_R     = ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 0  )) ? 5'b00000 :
+                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 1  )) ? 5'b00001 : 
+                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 2  )) ? 5'b00010 :    
+                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 3  )) ? 5'b00100 :    
+                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 4  )) ? 5'b01000 :    
+                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 5  )) ? 5'b10000 :  5'b00000;
+
+    assign  LCD_G    =  ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 6  )) ? 6'b000001: 
+                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 7  )) ? 6'b000010:    
+                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 8  )) ? 6'b000100:    
+                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 9  )) ? 6'b001000:    
+                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 10 )) ? 6'b010000:    
+                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 11 )) ? 6'b100000:  6'b000000;
+
+    assign  LCD_B    =  ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 12 )) ? 5'b00001 : 
+                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 13 )) ? 5'b00010 :    
+                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 14 )) ? 5'b00100 :    
+                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 15 )) ? 5'b01000 :    
+                        ( H_PixelCount < ( H_BackPorch +  Colorbar_width * 16 )) ? 5'b10000 :  5'b00000;
+
+endmodule
+
 
 module board_specific_top
 # (
@@ -54,13 +137,14 @@ module board_specific_top
     // Some LCD pins share bank with TMDS pins
     // which have different voltage requirements.
     //
-    // However we can use LCD_DEN, VS, HS, CK,
+    // However we can use LCD_CLK, DE, VS, HS, BL
     // because they are assigned to a different bank.
 
-    output                       LCD_DEN,
+    output                       LCD_CLK,
+    output                       LCD_DE,
     output                       LCD_VS,
     output                       LCD_HS,
-    output                       LCD_CLK,
+    output                       LCD_BL,
 
     output [7:7 + 1 - w_red   ]  LCD_R,
     output [7:7 + 1 - w_green ]  LCD_G,
@@ -108,6 +192,30 @@ module board_specific_top
     // On-board WS2812 RGB LED with a serial interface
     inout                        WS2812
 );
+
+/*
+    Gowin_rPLL Gowin_rPLL_9Mhz(
+        .clkout(LCD_CLK), // 9MHz
+        .clkin(CLK)   //27MHz
+    );
+
+        lcd_timing      lcd_timing_inst(
+                .PixelClk       (       LCD_CLK          ),
+                .nRST           (     ~ KEY [0]          ),
+
+                .LCD_DE         (       LCD_DE          ),
+
+                .LCD_B          (       LCD_B            ),
+                .LCD_G          (       LCD_G            ),
+                .LCD_R          (       LCD_R            )
+        );
+
+    assign LCD_VS = 1'b0;
+    assign LCD_HS = 1'b0;
+    assign LCD_BL = 1'b1;
+*/
+
+//`ifdef UNDEFINED
 
     wire clk = CLK;
 
@@ -161,7 +269,7 @@ module board_specific_top
         wire rst_on_power_up;
         imitate_reset_on_power_up i_reset_on_power_up (clk, rst_on_power_up);
 
-        wire rst = rst_on_power_up | (| (~ KEY));
+        wire rst = rst_on_power_up | (| KEY);
 
     `elsif IMITATE_RESET_ON_POWER_UP_FOR_TWO_BUTTON_CONFIGURATION
 
@@ -173,9 +281,9 @@ module board_specific_top
     `else  // Reset using an on-board button
 
         `ifdef REVERSE_KEY
-            wire rst = ~ KEY [0];
+            wire rst = KEY [0];
         `else
-            wire rst = ~ KEY [w_key - 1];
+            wire rst = KEY [w_key - 1];
         `endif
 
     `endif
@@ -194,9 +302,9 @@ module board_specific_top
     `else  // `ifdef INSTANTIATE_TM1638_BOARD_CONTROLLER_MODULE
 
         `ifdef REVERSE_KEY
-            `SWAP_BITS (lab_key, ~ KEY);
+            `SWAP_BITS (lab_key, KEY);
         `else
-            assign lab_key = ~ KEY;
+            assign lab_key = KEY;
         `endif
 
         //--------------------------------------------------------------------
@@ -319,7 +427,7 @@ module board_specific_top
 
         `else  // Using 480x272
 
-            Gowin_rPLL Gowin_rPLL
+            Gowin_rPLL i_Gowin_rPLL
             (
                 .clkout ( LCD_CLK ),  //  9 MHz
                 .clkin  ( clk     )   // 27 MHz
@@ -341,7 +449,7 @@ module board_specific_top
             .PixelClk  (   LCD_CLK        ),
             .nRST      ( ~ rst            ),
 
-            .LCD_DE    (   LCD_DEN        ),
+            .LCD_DE    (   LCD_DE         ),
             .LCD_HSYNC (                  ),
             .LCD_VSYNC (                  ),
 
@@ -349,11 +457,10 @@ module board_specific_top
             .y         (   y              )
         );
 
-        assign LCD_HSYNC = 1'b0;
-        assign LCD_VSYNC = 1'b0;
+        assign LCD_HS = 1'b0;
+        assign LCD_VS = 1'b0;
 
-        assign LCD_INIT  = 1'b0;
-        assign LCD_BL    = 1'b0;
+        assign LCD_BL = 1'b1;
 
     `endif
 
@@ -404,5 +511,7 @@ module board_specific_top
         assign PA_EN = 1'b1;
 
     `endif
+
+//`endif
 
 endmodule
