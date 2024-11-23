6412dec6835ace413fa84214971bb4847f6bfb57
Yuri Panchul <yuri@panchul.com>
2024-11-22 19:40:17 -0800

    Started creating a prototype for hackathon

--- labs/9_events/2025_hackathon/prototype/lab_top.sv
+++ labs/9_events/2025_hackathon/prototype/lab_top.sv
@@ -1,97 +1,36 @@
-`include "config.svh"
+// This module assumes the configuration with Tang Nano 9K board,
+// LCD 480x272 and TM1638 interface module.
 
-module lab_top
-# (
-    parameter  clk_mhz       = 50,
-               w_key         = 4,
-               w_sw          = 8,
-               w_led         = 8,
-               w_digit       = 8,
-               w_gpio        = 100,
-
-               screen_width  = 640,
-               screen_height = 480,
-
-               w_red         = 4,
-               w_green       = 4,
-               w_blue        = 4,
-
-               w_x           = $clog2 ( screen_width  ),
-               w_y           = $clog2 ( screen_height )
-)
+module hackathon_top
 (
-    input                        clk,
-    input                        slow_clk,
-    input                        rst,
+    input  logic       clock,
+    input  logic       reset,
 
-    // Keys, switches, LEDs
+    output logic [9:0] strobe_hz,
+    input  logic       strobe,
 
-    input        [w_key   - 1:0] key,
-    input        [w_sw    - 1:0] sw,
-    output logic [w_led   - 1:0] led,
+    input        [7:0] key,
+    output logic [7:0] led,
+    output logic [7:0] number,
 
-    // A dynamic seven-segment display
+    input  logic [8:0] x,
+    input  logic [8:0] y,
 
-    output logic [          7:0] abcdefgh,
-    output logic [w_digit - 1:0] digit,
-
-    // Graphics
-
-    input        [w_x     - 1:0] x,
-    input        [w_y     - 1:0] y,
-
-    output logic [w_red   - 1:0] red,
-    output logic [w_green - 1:0] green,
-    output logic [w_blue  - 1:0] blue,
-
-    // Microphone, sound output and UART
-
-    input        [         23:0] mic,
-    output       [         15:0] sound,
-
-    input                        uart_rx,
-    output                       uart_tx,
-
-    // General-purpose Input/Output
-
-    inout        [w_gpio  - 1:0] gpio
+    output logic [4:0] red,
+    output logic [5:0] green,
+    output logic [4:0] blue
 );
 
-    //------------------------------------------------------------------------
+    logic [7:0] counter;
 
-    // assign led        = '0;
-    // assign abcdefgh   = '0;
-    // assign digit      = '0;
-    // assign red        = '0;
-    // assign green      = '0;
-    // assign blue       = '0;
-       assign sound      = '0;
-       assign uart_tx    = '1;
+    always_ff @ (posedge clock)
+        if (reset)
+            counter <= 0;
+        else if (strobe)
+            counter <= counter + 1 + key [0] - key [1];
 
-    //------------------------------------------------------------------------
-    //
-    //  NOTE! Since the 9_maker_faire series of Verilog examples
-    //  is for absolute beginners,
-    //  we are using a simplified, more relaxed Verilog style
-    //  that may have many lint issues. For example,
-    //  we use "100" instead of "w_x' (100)" or "screen_width / 2".
-    //
-    //------------------------------------------------------------------------
-
-    logic pulse;
-
-    strobe_gen # (.clk_mhz (clk_mhz), .strobe_hz (30))
-    i_strobe_gen (clk, rst, pulse);
-
-    logic [7:0] c;
-
-    always_ff @ (posedge clk or posedge rst)
-        if (rst)
-            c <= 0;
-        else if (pulse)
-            c <= c + 1 + key [0] - key [1];
-
-    assign led = c;
+    assign led    = counter;
+    assign number = counter;
 
     always_comb
     begin
@@ -111,16 +50,4 @@ module lab_top
             green = 10;
     end
 
-    seven_segment_display # (w_digit) i_7segment
-    (
-        .clk      ( clk      ),
-        .rst      ( rst      ),
-        .number   ( c        ),
-        .dots     ( 0        ),
-        .abcdefgh ( abcdefgh ),
-        .digit    ( digit    )
-    );
-
 endmodule
-
-
