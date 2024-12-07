35be4b6c52b0bae837c4f06fbb7badad464ea4b9
Vadim Ostrikov <ovip7@mail.ru>
2024-12-07 12:56:20 +0400

    Check  python3

--- labs/3_music/3_3_note_synthesizer/gen_tone_table.sh
+++ labs/3_music/3_3_note_synthesizer/gen_tone_table.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+PY3=python3
+$PY3 --version || PY3=python
+
 WIDTH=${1:-16}
 
 VOLUME=${1:-14}
@@ -9,17 +12,17 @@ rm -f tone_table.svh
 sampling_rate=48828
 for note in C Cs D Ds E F Fs G Gs A As B; do
     echo "Generating LUT for $sampling_rate Hz, $note, $WIDTH-bit, Volume $VOLUME/15 bit..."
-    python gen.py --note $note $WIDTH $sampling_rate $VOLUME>> tone_table.svh
+    $PY3 gen.py --note $note $WIDTH $sampling_rate $VOLUME>> tone_table.svh
 done
 
 sampling_rate=64453
 for note in C Cs D Ds E F Fs G Gs A As B; do
     echo "Generating LUT for $sampling_rate Hz, $note, $WIDTH-bit, Volume $VOLUME/15 bit..."
-    python gen.py --note $note $WIDTH $sampling_rate $VOLUME>> tone_table.svh
+    $PY3 gen.py --note $note $WIDTH $sampling_rate $VOLUME>> tone_table.svh
 done
 
 sampling_rate=52734
 for note in C Cs D Ds E F Fs G Gs A As B; do
     echo "Generating LUT for $sampling_rate Hz, $note, $WIDTH-bit, Volume $VOLUME/15 bit..."
-    python gen.py --note $note $WIDTH $sampling_rate $VOLUME>> tone_table.svh
+    $PY3 gen.py --note $note $WIDTH $sampling_rate $VOLUME>> tone_table.svh
 done
