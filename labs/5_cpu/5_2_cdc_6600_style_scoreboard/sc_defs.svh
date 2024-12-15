68ba5eb84e2a1f40c07371eb4b2cca6997ca25a0
Yuri Panchul <yuri@panchul.com>
2024-12-14 23:55:14 -0800

    developing 5_2_cdc_6600_style_scoreboard

--- labs/5_cpu/5_2_cdc_6600_style_scoreboard/sc_defs.svh
+++ labs/5_cpu/5_2_cdc_6600_style_scoreboard/sc_defs.svh
@@ -14,23 +14,26 @@
 `ifndef SC_DEFS_SVH
 `define SC_DEFS_SVH
 
-localparam w_op      = 4,
-           w_reg_idx = 4,
-           w_reg_dat = 8,
-           w_imm     = w_reg_dat,
-           w_ins     = 16;
+localparam w_op      = 4,                // Operation code width
+           n_regs    = 16,               // Number of registers
+           w_reg_idx = $clog2 (n_regs),  // Register index width
+           w_reg_dat = 8,                // Register data width
+           w_imm     = w_reg_dat,        // Immediate value width
+           w_ins     = 16,               // Instruction width
+
+           n_fus     = 16,               // Number of Functional Units
+           w_fu_id   = $clog2 (n_fus);   // Functional Unit ID
 
 //----------------------------------------------------------------------------
 
 typedef enum bit [w_op - 1:0]
 {
-    I_NOP  = 0,
-    I_LI   = 1,  // Load Immediate
-    I_ADD  = 2,
-    I_SUB  = 3,
-    I_MUL  = 4,
-    I_DIV  = 5,
-    I_SQRT = 6
+    I_LI   = 0,  // Load Immediate
+    I_ADD  = 1,
+    I_SUB  = 2,
+    I_MUL  = 3,
+    I_DIV  = 4,
+    I_SQRT = 5
 }
 op_e;
 
@@ -40,6 +43,35 @@ typedef logic [w_reg_idx - 1:0] reg_idx_t;
 typedef logic [w_reg_dat - 1:0] reg_dat_t;
 typedef logic [w_imm     - 1:0] imm_t;
 typedef logic [w_ins     - 1:0] ins_t;
+typedef logic [w_fu_id   - 1:0] fu_id_t;
+
+//----------------------------------------------------------------------------
+
+typedef struct
+{
+    logic     busy;
+    op_e      op;
+
+    reg_idx_t dst_reg;                 // H&P refers as Fi
+    reg_idx_t src_reg_0;               // Fj
+    reg_idx_t src_reg_1;               // Fk
+
+    fu_id_t   fu_producing_src_reg_0;  // Qj
+    fu_id_t   fu_producing_src_reg_1;  // Qk
+
+    logic     fu_0_rdy;                // Rj
+    logic     fu_1_rdy;                // Rk
+}
+fu_status_t;
+
+//----------------------------------------------------------------------------
+
+typedef struct
+{
+    logic     result_pending;
+    fu_id_t   fu;
+}
+reg_status_t;
 
 //----------------------------------------------------------------------------
 
@@ -55,7 +87,7 @@ function [w_ins - 1:0] make_ins_rr (op_e op, reg_idx_t rd, rs);
     return { op, rd, rs, w_reg' (0) };
 endfunction
 
-function [w_ins - 1:0] make_ins_rr (op_e op, reg_idx_t rd, rs0, rs1);
+function [w_ins - 1:0] make_ins_rrr (op_e op, reg_idx_t rd, rs0, rs1);
     return { op, rd, rs0, rs1 };
 endfunction
 
