59597e1d0f91ae56b5af89ea3c862248c06f62e0
Yuri Panchul <yuri@panchul.com>
2024-12-25 10:08:29 -0800

    Cosmetic changes in i2s_audio_out.sv

--- peripherals/i2s_audio_out.sv
+++ peripherals/i2s_audio_out.sv
@@ -1,12 +1,31 @@
-// For PCM5102A pin XSMT should be connected to 3.3v, pins FLT, DEMP, FMT to GND
-// For Digilent Pmod AMP3 jumper JP3 is loaded, I2S mode
+// For PCM5102A pin XSMT should be connected to 3.3v,
+// pins FLT, DEMP, FMT to GND.
+//
+// For Digilent Pmod AMP3 jumper JP3 is loaded, I2S mode.
 
 module i2s_audio_out
 # (
     parameter clk_mhz             = 50,
-              in_res              = 16,   // Sound samples resolution
-              align_right         = 1'b0, // For I2S = 0 For PT8211 DAC (Least Significant Bit Justified) = 1
-              offset_by_one_cycle = 1'b1  // For I2S = 1 For Left Justified Audio Data Format = 0
+              in_res              = 16,  // Sound sample resolution
+
+              // For the standard I2S, align_right = 0,
+              // i.e. value is aligned to the left relative to LRCLK signal,
+              // MSB - Most Significant Bit Justified.
+
+              // For PT8211 DAC, align_right = 1,
+              // i.e. value is aligned to the right relative to LRCLK signal,
+              // LSB - Least Significant Bit Justified.
+
+              align_right         = 0,
+
+              // For the standard I2S, offset_by_one_cycle = 1,
+              // i.e. value transmission starts with an offset of 1 clock cycle
+              // relative to LRCLK signal
+
+              // For PT8211 DAC, offset_by_one_cycle = 0,
+              // i.e. value transmission is aligned with LRCLK signal change.
+
+              offset_by_one_cycle = 1
 )
 (
     input                 clk,
@@ -18,40 +37,77 @@ module i2s_audio_out
     output                sdata
 );
 
-// Standard frequencies are 12.288 MHz, 3.072 MHz and 48 KHz. 
-// We are using frequencies somewhat higher but with the same relationship 256:64:1
-    localparam MCLK_BIT   =  $clog2 ( clk_mhz - 4 ) - 4;  // clk_mhz range (12-19) (20-35) (36-67) (68-131)
-    localparam BCLK_BIT   =  MCLK_BIT + 2;
-    localparam LRCLK_BIT  =  BCLK_BIT + 6;
+    // Number of bits in one transmission during half a period of LRCLK
+ 
+    localparam W_OUT_VALUE = 32;
 
-    logic  [LRCLK_BIT - 1:0] clk_div;
-    logic  [           31:0] shift;
-    logic  [           31:0] data_aligned;
+    // Standard frequencies are 12.288 MHz, 3.072 MHz and 48 KHz. 
+    // We are using frequencies somewhat higher
+    // but with the same relationship 256:64:1.
 
-    assign mclk  = MCLK_BIT ? clk_div [MCLK_BIT - 1] : clk;
-    assign bclk  = clk_div [BCLK_BIT  - 1];
-    assign lrclk = clk_div [LRCLK_BIT - 1];
+    // We are grouping together clk_mhz ranges of
+    // (12-19), (20-35), (36-67), (68-131).
 
-    assign sdata = shift   [           31];
+    localparam MCLK_BIT  =  $clog2 (clk_mhz - 4) - 4,
+               BCLK_BIT  =  MCLK_BIT + 2,
+               LRCLK_BIT =  BCLK_BIT + 6;
 
-// Put the data on the right side of LRCLK or WS or put the data starting with the highest bytes on the left side
-    assign data_aligned = align_right ? 32' ( data_in ) : {data_in, {32 - in_res {1'b0}}};
+    logic  [LRCLK_BIT   - 1:0] clk_div;
+    logic  [W_OUT_VALUE - 1:0] shift;
+    logic  [W_OUT_VALUE - 1:0] data_aligned;
 
-    always_ff @ ( posedge clk or posedge reset )
-        if ( reset )
+    generate
+
+        if (MCLK_BIT)
+        begin : gen_MCLK_BIT
+            assign mclk = clk_div [MCLK_BIT - 1];
+        end
+        else
+        begin : gen_not_MCLK_BIT
+            assign mclk = clk;
+        end
+
+    endgenerate
+
+    assign bclk  = clk_div [BCLK_BIT    - 1];
+    assign lrclk = clk_div [LRCLK_BIT   - 1];
+    assign sdata = shift   [W_OUT_VALUE - 1];
+
+    // Put data right-aligned relative to LRCLK (sometimes called WS)
+    // or align data to the left, with the most significant bit, MSB.
+
+    assign data_aligned
+        = align_right ?
+              W_OUT_VALUE' (data_in)
+            : { data_in, { W_OUT_VALUE - in_res { 1'b0 } } };
+
+    always_ff @ (posedge clk or posedge reset)
+        if (reset)
             clk_div <= '0;
         else
-            clk_div <= clk_div + 1'b1;
+            clk_div <= clk_div + 1'd1;
 
-    always_ff @ ( posedge clk or posedge reset )
-        if ( reset )
+    always_ff @ (posedge clk or posedge reset)
+    begin
+        if (reset)
+        begin
             shift <= '0;
+        end
         else
         begin
-            if      ( clk_div [LRCLK_BIT - 2:0] == { { 5 { ~offset_by_one_cycle } }, { BCLK_BIT { 1'b1 } } } )
-                              shift <= data_aligned; 
-            else if ( clk_div [BCLK_BIT  - 1:0] == '1 )
-                              shift <= shift << 1'b1;    // Serial shift on the left side
+            if (clk_div [LRCLK_BIT - 2:0]
+                ==
+                { { LRCLK_BIT - BCLK_BIT - 1 { ~ offset_by_one_cycle } },
+                  { BCLK_BIT                 {   1'b1                } } } )
+            begin
+                shift <= data_aligned;
+            end
+            else if (clk_div [BCLK_BIT - 1:0] == '1)
+            begin
+                // Shift to generate serial data
+                shift <= shift << 1;
+            end
         end
+    end
 
 endmodule
