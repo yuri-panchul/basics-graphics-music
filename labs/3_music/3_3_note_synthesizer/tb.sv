c6773e48b56a12f2e2368220dafa259efc754335
Yuri Panchul <yuri@panchul.com>
2024-12-25 11:36:14 -0800

    Improved simulation for sound output

--- labs/3_music/3_3_note_synthesizer/tb.sv
+++ labs/3_music/3_3_note_synthesizer/tb.sv
@@ -7,14 +7,19 @@ module tb;
                w_sw    = 4,
                w_led   = 8,
                w_digit = 8,
+               w_sound = 16,
                w_gpio  = 100;
+
+    // Based on timescale is 1 ns / 1 ps
+    localparam clk_period = (1_000_000_000 / 1_000_000) / clk_mhz;
 
     //------------------------------------------------------------------------
 
-    logic       clk;
-    logic       rst;
-    logic [3:0] key;
-    logic [3:0] sw;
+    logic                 clk;
+    logic                 rst;
+    logic [w_key   - 1:0] key;
+    logic [w_sw    - 1:0] sw;
+    logic [w_sound - 1:0] sound;
 
     //------------------------------------------------------------------------
 
@@ -25,15 +30,56 @@ module tb;
         .w_sw    ( w_sw    ),
         .w_led   ( w_led   ),
         .w_digit ( w_digit ),
+        // TODO .w_sound ( w_sound ),
         .w_gpio  ( w_gpio  )
     )
     i_lab_top
     (
-        .clk      ( clk ),
-        .slow_clk ( clk ),
-        .rst      ( rst ),
-        .key      ( key ),
-        .sw       ( sw  )
+        .clk      ( clk    ),
+        .slow_clk ( clk    ),
+        .rst      ( rst    ),
+        .key      ( key    ),
+        .sw       ( sw     ),
+        .sound    ( sound  )
+    );
+
+    //------------------------------------------------------------------------
+
+    wire mclk, bclk, lrclk, sdata;
+
+    i2s_audio_out
+    # (
+        .clk_mhz  ( clk_mhz ),
+        .in_res   ( w_sound ),
+
+        // For the standard I2S, align_right = 0,
+        // i.e. value is aligned to the left relative to LRCLK signal,
+        // MSB - Most Significant Bit Justified.
+
+        // For PT8211 DAC, align_right = 1,
+        // i.e. value is aligned to the right relative to LRCLK signal,
+        // LSB - Least Significant Bit Justified.
+
+        .align_right (0),
+
+        // For the standard I2S, offset_by_one_cycle = 1,
+        // i.e. value transmission starts with an offset of 1 clock cycle
+        // relative to LRCLK signal
+
+        // For PT8211 DAC, offset_by_one_cycle = 0,
+        // i.e. value transmission is aligned with LRCLK signal change.
+
+        .offset_by_one_cycle (1)
+    )
+    i_i2s_audio_out
+    (
+        .clk      ( clk    ),
+        .reset    ( rst    ),
+        .data_in  ( sound  ),
+        .mclk     ( mclk   ),
+        .bclk     ( bclk   ),
+        .lrclk    ( lrclk  ),
+        .sdata    ( sdata  )
     );
 
     //------------------------------------------------------------------------
@@ -43,7 +89,7 @@ module tb;
         clk = 1'b0;
 
         forever
-            # 1 clk = ! clk;
+            # (clk_period / 2) clk = ~ clk;
     end
 
     //------------------------------------------------------------------------
@@ -63,12 +109,13 @@ module tb;
 
     initial
     begin
-        #0
         `ifdef __ICARUS__
             $dumpvars;
         `endif
 
-        #400000
+        // Based on timescale is 1 ns / 1 ps
+
+        # 1_000_000
 
         `ifdef MODEL_TECH  // Mentor ModelSim and Questa
             $stop;
