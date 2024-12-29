bb187bf6033482257a596b2b4d284a623dbe90b2
Yuri Panchul <yuri@panchul.com>
2024-12-29 13:50:53 -0800

    Added exercises

---
+++ labs/9_events/2025_tijuana/day_1_pm_graphics_using_combination_logic/1_red_rectangle/hackathon_top.sv
@@ -0,0 +1,37 @@
+// Board configuration: tang_nano_9k_lcd_480_272_tm1638_hackathon
+// This module uses few parameterization and relaxed typing rules
+
+module hackathon_top
+(
+    input  logic       clock,
+    input  logic       reset,
+
+    input  logic [7:0] key,
+    output logic [7:0] led,
+
+    // A dynamic seven-segment display
+
+    output logic [7:0] abcdefgh,
+    output logic [7:0] digit,
+
+    // LCD screen interface
+
+    input  logic [8:0] x,
+    input  logic [8:0] y,
+
+    output logic [4:0] red,
+    output logic [5:0] green,
+    output logic [4:0] blue
+);
+
+    always_comb
+    begin
+        red   = 0;
+        green = 0;
+        blue  = 0;
+
+        if (x > 100 & x < 150 & y > 100 & y < 200)
+            red = 30;
+    end
+
+endmodule
