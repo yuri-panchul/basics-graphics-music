9b5a5688cdf068248b3d5d9ebd990aed4d207ca4
sushemer <diego8tjmx@gmail.com>
2025-01-15 08:22:50 -0800

    Update hackathon_top.sv 2 change color
    Peralta Martinez Diego Sebastian

--- labs/9_events/2025_tijuana/day_1_pm_graphics_using_combination_logic/2_change_color_with_keys/hackathon_top.sv
+++ labs/9_events/2025_tijuana/day_1_pm_graphics_using_combination_logic/2_change_color_with_keys/hackathon_top.sv
@@ -30,8 +30,17 @@ module hackathon_top
         green = 0;
         blue  = 0;
 
-        if (x > 100 & x < 300 & y > 50 & y < 100)
-            red = 31;
+if (x > 100 & x < 300 & y > 100 & y < 150)
+            if(key[0]) //red=yellow and purple=white
+                begin
+                    red=31;
+                    green=63;
+                    blue=0;
+                end
+            else 
+                begin        
+                    red = 31;
+                end
 
         // 31 is the maximum 5-bit number, 5'b11111
 
@@ -39,15 +48,33 @@ module hackathon_top
         // that overlaps red rectangle
 
         if (x > 150 & x < 350 & y > 70 & y < 120)
-            green = 63;
+            if(key[1]) // green,red and blue= sky blue
+                begin
+                    red=0;
+                    green=63;
+                    blue=31;
+                end
+            else 
+                begin        
+                    green = 63;
+                end
 
         // 63 is the maximum 6-bit number, 6'b111111
 
         // Exercise 2: Add a blue rectangle
         // that overlaps both rectangles
 
-        if (x > 200 & x < 400 & y > 90 & y < 140)
-            blue = 31;
+        if (x > 200 & x < 400 & y > 100 & y < 150)
+            if(key[2]) //full blue
+                begin
+                    red=0;
+                    green=0;
+                    blue=31;
+                end
+            else 
+                begin        
+                    blue = 31;
+                end
     end
 
 endmodule
