3d74e4a470e2b0c0dbc564f98eb0f8512a326375
sushemer <diego8tjmx@gmail.com>
2025-01-15 08:20:48 -0800

    Update hackathon_top.sv

--- labs/9_events/2025_tijuana/day_1_am_combination_logic_basics/4_seven_segment_letter/hackathon_top.sv
+++ labs/9_events/2025_tijuana/day_1_am_combination_logic_basics/4_seven_segment_letter/hackathon_top.sv
@@ -32,44 +32,56 @@ module hackathon_top
     //  e     c     1     0
     //  |     |     |      
     //   --d--  h      0    0
-
-    typedef enum bit [7:0]
+typedef enum bit [7:0]
     {
         //         abcd efgh
-        F     = 8'b1000_1110,  // This means "8-bit binary number"
+        R     = 8'b1100_1100,  // This means "8-bit binary number"
         P     = 8'b1100_1110,
-        G     = 8'b1011_1100,
+        E     = 8'b1001_1110,
         A     = 8'b1110_1110,
+        F     = 8'b1000_1110,  // This means "8-bit binary number"
+        G     = 8'b1011_1110,
+        B     = 8'b0011_1110,
+        S     = 8'b1011_0111,
+        T     = 8'b0011_1100,
         space = 8'b0000_0000
     }
     seven_seg_encoding_e;
 
-    assign abcdefgh = key [0] ? P : F;
-    assign digit    = key [1] ? 2'b10 : 2'b01;
+    //assign abcdefgh = key [0] ? P : R;
+    //assign digit    = key [1] ? 2'b10 : 2'b01;
 
     // Exercise 1: Display the first letters
     // of your first name and last name instead.
 
     // assign abcdefgh = ...
     // assign digit    = ...
+    
+    /*
+    assign abcdefgh = key[3] ? P : (key[2] ? E : (key[1] ? R : (key[0] ? A : space)));
+    assign digit = (key[3]) ? 4'b1000 :   // se enciende 5
+               (key[2]) ? 4'b0100 :   // se enciende 6
+               (key[1]) ? 4'b0010 :   // se enciende 7
+               (key[0]) ? 4'b0001 :   // se enciende 8
+               4'b0000;               //apagar
+    */
+
 
     // Exercise 2: Display letters of a 4-character word
     // using this code to display letter of FPGA as an example
 
-    /*
+    
     seven_seg_encoding_e letter;
 
     always_comb
       case (key)
-      4'b1000: letter = F;
-      4'b0100: letter = P;
-      4'b0010: letter = G;
-      4'b0001: letter = A;
+      4'b1000: letter = B;
+      4'b0100: letter = A;
+      4'b0010: letter = S;
+      4'b0001: letter = T;
       default: letter = space;
       endcase
 
     assign abcdefgh = letter;
     assign digit    = key;
-    */
-
 endmodule
