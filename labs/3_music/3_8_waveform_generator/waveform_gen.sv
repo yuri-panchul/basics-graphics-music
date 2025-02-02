cbf788016e68b16ceb2b08ffe778bae069651305
Vadim Ostrikov <ovip7@mail.ru>
2025-02-02 23:27:46 +0400

    The code is made simpler

--- labs/3_music/3_8_waveform_generator/waveform_gen.sv
+++ labs/3_music/3_8_waveform_generator/waveform_gen.sv
@@ -9,9 +9,9 @@ module waveform_gen
 (
     input                         clk,
     input                         reset,
-    input  [                 2:0] octave,
-    input  [                 3:0] waveform, // waveform type
-    output [y_width        - 1:0] y
+    input        [           2:0] octave,
+    input        [           3:0] waveform, // waveform type
+    output logic [y_width  - 1:0] y
 );
 
     // We are grouping together clk_mhz ranges of
@@ -34,9 +34,6 @@ module waveform_gen
     logic signed [y_width - 1:0] yt   = '0;
     logic        [y_width - 1:0] ys;
     logic        [y_width - 1:0] yq;
-    logic        [y_width - 1:0] yg;
-
-    assign y = yg;
 
     always_ff @ (posedge clk or posedge reset)
         if (reset)
@@ -68,10 +65,10 @@ module waveform_gen
 
     always_comb begin
         case (waveform)
-           'b0001: yg = ys; // sinus
-           'b0010: yg = yt; // triangle
-           'b0100: yg = yq; // square
-          default: yg = '0;
+           'b0001: y = ys; // sinus
+           'b0010: y = yt; // triangle
+           'b0100: y = yq; // square
+          default: y = '0;
         endcase
     end
 
@@ -103,47 +100,11 @@ endmodule
 
 module sinus
 (
-    input      [15:0] y_max,
-    input      [15:0] yt,
-    output     [15:0] ys
+    input  logic    [15:0] y_max,
+    input  logic    [15:0] yt,
+    output logic    [15:0] ys
 );
     localparam [15:0] MAX = '1;
-
-    logic      [15:0] yt1;
-    logic      [15:0] yt2;
-    logic      [15:0] yt3;
-    logic      [15:0] yt4;
-    logic      [15:0] yt5;
-    logic      [15:0] yt6;
-    logic      [15:0] yt_inv;
-
-    logic      [15:0] y_max1;
-    logic      [15:0] y_max2;
-    logic      [15:0] y_max3;
-    logic      [15:0] y_max4;
-    logic      [15:0] y_max5;
-    logic      [15:0] y_max6;
-    logic      [15:0] y_max7;
-    logic      [15:0] y_max8;
-
-    // Shifting to right >> 1 is equivalent to dividing by 2
-
-    assign yt_inv = (MAX - yt);         // inverted
-    assign yt1    = (yt  >> 1) - (yt >> 6);
-    assign yt2    = (yt  >> 1) + (yt >> 3);
-    assign yt3    = (yt2 >> 2);
-    assign yt4    = (yt_inv >> 1) - (yt_inv >> 6);
-    assign yt5    = (yt_inv >> 1) + (yt_inv >> 3);
-    assign yt6    = (yt5 >> 2);
-
-    assign y_max1 = (y_max  >> 1);
-    assign y_max2 =  y_max1 + (y_max >> 3);
-    assign y_max3 = (y_max  >> 2) - (y_max >> 5) - (y_max >> 6);
-    assign y_max4 =  y_max2 + (y_max >> 2);
-    assign y_max5 = (y_max4 >> 1);
-    assign y_max6 =  y_max1 + (y_max >> 2);
-    assign y_max7 = (y_max6 >> 3);
-    assign y_max8 =  y_max6 +  y_max7;
 
     // Made as analog waveform generator ICL8038.
     // The sine wave is created by feeding the triangle wave into a
@@ -151,21 +112,31 @@ module sinus
     // decreasing transmission ratio as the level of the triangle
     // moves toward the two extremes. Accuracy of the sine is 1%.
 
-    assign ys = (yt > (MAX >> 1)) ?
-               ((yt > (MAX - y_max5)) ? // negative half-wave
-                      (yt  - yt4) :
-               ((yt > (MAX - y_max2)) ?
-                      (yt  - y_max3) :
-               ((yt > (MAX - y_max4)) ?
-                      (MAX - y_max5 - yt5) :
-                      (MAX - y_max8 - yt6)))) :
-               ((yt < y_max5) ?         // < 0.44 y_max
-                     (yt + yt1) :       //              + 1.48 yt
-               ((yt < y_max2) ?         // < 0.63 y_max
-                     (yt + y_max3) :    //   0.21 y_max + 1.00 yt
-               ((yt < y_max4) ?         // < 0.88 y_max
-                     (yt2 + y_max5) :   //   0.44 y_max + 0.63 yt
-                     (yt3 + y_max8)))); //   0.84 y_max + 0.16 yt
+    always_comb
+    begin
+    if     (yt > (MAX >> 1)) // Shifting to right >> 1 for positive numbers is equivalent to dividing by 2
+    begin
+        if       (yt >   MAX - (y_max >> 1) + (y_max >> 4))                   // negative half-wave
+            ys =  yt - ((MAX - yt) >> 1) + ((MAX - yt) >> 5);
+        else if  (yt >  (MAX - (y_max >> 1) - (y_max >> 3)))
+            ys =  yt + ((MAX - yt) >> 4) - (y_max >> 2);
+        else if  (yt >   MAX - (y_max >> 1) - (y_max >> 2) - (y_max >> 4))
+            ys = MAX - ((MAX - yt) >> 1) - ((MAX - yt) >> 4) - (y_max >> 1);
+        else
+            ys = MAX - ((MAX - yt) >> 3) - ((MAX - yt) >> 5) - y_max + (y_max >> 3) + (y_max >> 5);
+    end
+    else
+    begin
+        if       (yt < (y_max >> 1) - (y_max >> 4))                           // < 0.4375  y_max
+            ys =  yt + (yt >> 1) - (yt >> 5);                                 //                 + 1.46875 yt
+        else if  (yt < (y_max >> 1) + (y_max >> 3))                           // < 0.625   y_max
+            ys =  yt - (yt >> 4) + (y_max >> 2);                              //   0.25    y_max + 0.9375  yt
+        else if  (yt < (y_max >> 1) + (y_max >> 2) + (y_max >> 4))            // < 0.8125  y_max
+            ys = (yt >> 1) + (yt >> 4) + (y_max >> 1);                        //   0.5     y_max + 0.5625  yt
+        else
+            ys = (yt >> 3) + (yt >> 5) + y_max - (y_max >> 3) - (y_max >> 5); //   0.84375 y_max + 0.15625 yt
+    end
+    end
 
 endmodule
 
