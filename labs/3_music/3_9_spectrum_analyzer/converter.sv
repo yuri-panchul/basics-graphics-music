53f5ff9ad30141c2c2f44b60546764398f47eec4
Vadim Ostrikov <ovip7@mail.ru>
2025-02-23 19:49:13 +0400

    Reduced the number of LUTs

--- labs/3_music/3_9_spectrum_analyzer/converter.sv
+++ labs/3_music/3_9_spectrum_analyzer/converter.sv
@@ -1,12 +1,12 @@
 module converter 
 # (
-    parameter  stripe = 7,  // stripe narrower > 7, stripe wider < 7
-               level  = 15, // output level (shift)
-               smooth = 9   // smoothing output fluctuations
+    parameter                  stripe = 7,  // stripe narrower > 7 wider < 7
+                               level  = 15, // output level (shift)
+                               smooth = 9   // smoothing output fluctuations
 )
 (
-    input  logic  clk,
-    input  logic  rst,
+    input  logic               clk,
+    input  logic               rst,
     input  logic signed [ 8:0] mic,
     input  logic        [16:0] band_count,
     output logic        [10:0] rms_out
@@ -16,9 +16,9 @@ module converter
     logic               [16:0] count      = '0;
     logic                      pulse_out;
     logic               [ 4:0] switch     = '0;
-    logic  signed       [ 8:0] in, q00, q90;
-    logic  signed       [17:0] i_filtered = '0;
-    logic  signed       [17:0] q_filtered = '0;
+    logic        signed [ 8:0] in, q00, q90;
+    logic        signed [17:0] i_filtered = '0;
+    logic        signed [17:0] q_filtered = '0;
     logic               [17:0] abs_i, abs_q;
     logic               [18:0] sum_abs;
     logic               [21:0] ema        = '0;
@@ -52,163 +52,163 @@ module converter
             in  <= '0;
         end else begin
             in  <= mic;
-    //------------------------------------------------------------------------
+    //------------------------------------q00--9-h----q90-12-h----------------
         case (switch)
              0: begin 
             q00 <= '0;
             q90 <=  in;
             end
              1: begin 
-            q00 <= (in >>> 1) - (in >>> 6);
-            q90 <=  in - (in >>> 7);
+            q00 <=  in >>> 1;
+            q90 <=  in;
             end
              2: begin 
-            q00 <= (in >>> 1) + (in >>> 3) + (in >>> 5);
-            q90 <=  in - (in >>> 5);
+            q00 <= (in >>> 1) + (in >>> 3);
+            q90 <=  in;
             end
              3: begin 
-            q00 <=  in - (in >>> 2) + (in >>> 5);
-            q90 <=  in - (in >>> 4) - (in >>> 7);
+            q00 <=  in - (in >>> 2);
+            q90 <=  in;
             end
              4: begin 
-            q00 <=  in - (in >>> 3) - (in >>> 7);
-            q90 <=  in - (in >>> 3) - (in >>> 7);
+            q00 <=  in - (in >>> 3);
+            q90 <=  in - (in >>> 3);
             end
              5: begin 
-            q00 <=  in - (in >>> 4) - (in >>> 7);
-            q90 <=  in - (in >>> 2) + (in >>> 5);
+            q00 <=  in;
+            q90 <=  in - (in >>> 2);
             end
              6: begin 
-            q00 <=  in - (in >>> 5);
-            q90 <= (in >>> 1) + (in >>> 3) + (in >>> 5);
+            q00 <=  in;
+            q90 <= (in >>> 1) + (in >>> 3);
             end
              7: begin 
-            q00 <=  in - (in >>> 7);
-            q90 <= (in >>> 1) - (in >>> 6);
+            q00 <=  in;
+            q90 <=  in >>> 1;
             end
+    //------------------------------------q00-12-h----q90--3-h----------------
              8: begin 
             q00 <=  in;
             q90 <= '0;
             end
-    //------------------------------------------------------------------------
              9: begin 
-            q00 <=  in - (in >>> 7);
-            q90 <= - ((in >>> 1) - (in >>> 6));
+            q00 <=  in;
+            q90 <= - (in >>> 1);
             end
             10: begin 
-            q00 <=  in - (in >>> 5);
-            q90 <= - ((in >>> 1) + (in >>> 3) + (in >>> 5));
+            q00 <=  in;
+            q90 <= - ((in >>> 1) + (in >>> 3));
             end
             11: begin 
-            q00 <=  in - (in >>> 4) - (in >>> 7);
-            q90 <= - (in - (in >>> 2) + (in >>> 5));
+            q00 <=  in;
+            q90 <= - (in - (in >>> 2));
             end
             12: begin 
-            q00 <=  in - (in >>> 3) - (in >>> 7);
-            q90 <= - (in - (in >>> 3) - (in >>> 7));
+            q00 <=  in - (in >>> 3);
+            q90 <= - (in - (in >>> 3));
             end
             13: begin 
-            q00 <=  in - (in >>> 2) + (in >>> 5);
-            q90 <= - (in - (in >>> 4) - (in >>> 7));
+            q00 <=  in - (in >>> 2);
+            q90 <= - in;
             end
             14: begin 
-            q00 <= (in >>> 1) + (in >>> 3) + (in >>> 5);
-            q90 <= - (in - (in >>> 5));
+            q00 <= (in >>> 1) + (in >>> 3);
+            q90 <= - in;
             end
             15: begin 
-            q00 <= (in >>> 1) - (in >>> 6);
-            q90 <= - (in - (in >>> 7));
+            q00 <=  in >>> 1;
+            q90 <= - in;
             end
+    //------------------------------------q00--3-h----q90--6-h----------------
             16: begin 
             q00 <= '0;
             q90 <= - in;
             end
-    //------------------------------------------------------------------------
             17: begin 
-            q00 <= - ((in >>> 1) - (in >>> 6));
-            q90 <= - (in - (in >>> 7));
+            q00 <= - in >>> 1;
+            q90 <= - in;
             end
             18: begin 
-            q00 <= - ((in >>> 1) + (in >>> 3) + (in >>> 5));
-            q90 <= - (in - (in >>> 5));
+            q00 <= - ((in >>> 1) + (in >>> 3));
+            q90 <= - in;
             end
             19: begin 
-            q00 <= - (in - (in >>> 2) + (in >>> 5));
-            q90 <= - (in - (in >>> 4) - (in >>> 7));
+            q00 <= - (in - (in >>> 2));
+            q90 <= - in;
             end
             20: begin 
-            q00 <= - (in - (in >>> 3) - (in >>> 7));
-            q90 <= - (in - (in >>> 3) - (in >>> 7));
+            q00 <= - (in - (in >>> 3));
+            q90 <= - (in - (in >>> 3));
             end
             21: begin 
-            q00 <= - (in - (in >>> 4) - (in >>> 7));
-            q90 <= - (in - (in >>> 2) + (in >>> 5));
+            q00 <= - in;
+            q90 <= - (in - (in >>> 2));
             end
             22: begin 
-            q00 <= - (in - (in >>> 5));
-            q90 <= - ((in >>> 1) + (in >>> 3) + (in >>> 5));
+            q00 <= - in;
+            q90 <= - ((in >>> 1) + (in >>> 3));
             end
             23: begin 
-            q00 <= - (in - (in >>> 7));
-            q90 <= - ((in >>> 1) - (in >>> 6));
+            q00 <= - in;
+            q90 <= - (in >>> 1);
             end
+    //------------------------------------q00--6-h----q90--9-h----------------
             24: begin 
             q00 <= - in;
             q90 <= '0;
             end
-    //------------------------------------------------------------------------
             25: begin 
-            q00 <= - (in - (in >>> 7));
-            q90 <= (in >>> 1) - (in >>> 6);
+            q00 <= - in;
+            q90 <=  in >>> 1;
             end
             26: begin 
-            q00 <= - (in - (in >>> 5));
-            q90 <= (in >>> 1) + (in >>> 3) + (in >>> 5);
+            q00 <= - in;
+            q90 <= (in >>> 1) + (in >>> 3);
             end
             27: begin 
-            q00 <= - (in - (in >>> 4) - (in >>> 7));
-            q90 <= in - (in >>> 2) + (in >>> 5);
+            q00 <= - in;
+            q90 <=  in - (in >>> 2);
             end
             28: begin 
-            q00 <= - (in - (in >>> 3) - (in >>> 7));
-            q90 <= in - (in >>> 3) - (in >>> 7);
+            q00 <= - (in - (in >>> 3));
+            q90 <=  in - (in >>> 3);
             end
             29: begin 
-            q00 <= - (in - (in >>> 2) + (in >>> 5));
-            q90 <= in - (in >>> 4) - (in >>> 7);
+            q00 <= - (in - (in >>> 2));
+            q90 <=  in;
             end
             30: begin 
-            q00 <= - ((in >>> 1) + (in >>> 3) + (in >>> 5));
-            q90 <= in - (in >>> 5);
+            q00 <= - ((in >>> 1) + (in >>> 3));
+            q90 <=  in;
             end
             31: begin 
-            q00 <= - ((in >>> 1) - (in >>> 6));
-            q90 <= in - (in >>> 7);
+            q00 <= - (in >>> 1);
+            q90 <=  in;
             end
+    //------------------------------------q00--9-h----q90-12-h----------------
             default: begin 
             q00 <= '0;
             q90 <= '0;
             end
         endcase
-    //------------------------------------------------------------------------
         end
-        end
+    end
 
-    // RC filter I
+    // RC low frequency filter I
     always_ff @(posedge clk or posedge rst) begin
         if (rst)
             i_filtered <= '0;
-        else if (i_filtered [16] != i_filtered [17])
+        else if (i_filtered [16] != i_filtered [17])  // overflow prevention
             i_filtered <= {i_filtered [17], i_filtered [17], {16 {~i_filtered [17]}}};
         else if (pulse_out)
             i_filtered <= i_filtered - (i_filtered >>> stripe) + q00;
     end
 
-    // RC filter Q
+    // RC low frequency filter Q
     always_ff @(posedge clk or posedge rst) begin
         if (rst)
             q_filtered <= '0;
-        else if (q_filtered [16] != q_filtered [17])
+        else if (q_filtered [16] != q_filtered [17])  // overflow prevention
             q_filtered <= {q_filtered [17], q_filtered [17], {16 {~q_filtered [17]}}};
         else if (pulse_out)
             q_filtered <= q_filtered - (q_filtered >>> stripe) + q90;
--- labs/3_music/3_9_spectrum_analyzer/gtkwave.tcl
+++ labs/3_music/3_9_spectrum_analyzer/gtkwave.tcl
@@ -18,4 +18,59 @@ set num_added [ gtkwave::addSignalsFromList $all_signals ]
 
 gtkwave::/Time/Zoom/Zoom_Full
 
+gtkwave::highlightSignalsFromList "tb.i_converter.q00\[8:0\]"
+gtkwave::/Edit/Data_Format/Analog/Step
+gtkwave::/Edit/Data_Format/Signed_Decimal
+gtkwave::/Edit/Insert_Analog_Height_Extension
+
+gtkwave::highlightSignalsFromList "tb.i_converter.q90\[8:0\]"
+gtkwave::/Edit/Data_Format/Analog/Step
+gtkwave::/Edit/Data_Format/Signed_Decimal
+gtkwave::/Edit/Insert_Analog_Height_Extension
+
+gtkwave::highlightSignalsFromList "tb.i_converter.i_filtered\[17:0\]"
+gtkwave::/Edit/Data_Format/Analog/Step
+gtkwave::/Edit/Data_Format/Signed_Decimal
+gtkwave::/Edit/Insert_Analog_Height_Extension
+
+gtkwave::highlightSignalsFromList "tb.i_converter.q_filtered\[17:0\]"
+gtkwave::/Edit/Data_Format/Analog/Step
+gtkwave::/Edit/Data_Format/Signed_Decimal
+gtkwave::/Edit/Insert_Analog_Height_Extension
+
+gtkwave::highlightSignalsFromList "tb.i_converter.abs_i\[17:0\]"
+gtkwave::/Edit/Data_Format/Analog/Step
+gtkwave::/Edit/Data_Format/Decimal
+gtkwave::/Edit/Insert_Analog_Height_Extension
+
+gtkwave::highlightSignalsFromList "tb.i_converter.abs_q\[17:0\]"
+gtkwave::/Edit/Data_Format/Analog/Step
+gtkwave::/Edit/Data_Format/Decimal
+gtkwave::/Edit/Insert_Analog_Height_Extension
+
+gtkwave::highlightSignalsFromList "tb.i_converter.sum_abs\[18:0\]"
+gtkwave::/Edit/Data_Format/Analog/Step
+gtkwave::/Edit/Data_Format/Decimal
+gtkwave::/Edit/Insert_Analog_Height_Extension
+
+gtkwave::highlightSignalsFromList "tb.i_converter.ema\[21:0\]"
+gtkwave::/Edit/Data_Format/Analog/Step
+gtkwave::/Edit/Data_Format/Decimal
+gtkwave::/Edit/Insert_Analog_Height_Extension
+
+gtkwave::highlightSignalsFromList "tb.i_converter.rms_out\[10:0\]"
+gtkwave::/Edit/Data_Format/Analog/Step
+gtkwave::/Edit/Data_Format/Decimal
+gtkwave::/Edit/Insert_Analog_Height_Extension
+
+gtkwave::highlightSignalsFromList "tb.i_converter.mic\[8:0\]"
+gtkwave::/Edit/Data_Format/Analog/Step
+gtkwave::/Edit/Data_Format/Signed_Decimal
+gtkwave::/Edit/Insert_Analog_Height_Extension
+
+gtkwave::highlightSignalsFromList "tb.i_converter.switch\[4:0\]"
+gtkwave::/Edit/Data_Format/Analog/Step
+gtkwave::/Edit/Data_Format/Decimal
+gtkwave::/Edit/Insert_Analog_Height_Extension
+
 gtkwave::/Edit/UnHighlight_All
