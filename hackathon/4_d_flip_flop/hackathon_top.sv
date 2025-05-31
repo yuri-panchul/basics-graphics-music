844d5237880d4afa993abb6aaad40c47894345cf
Yuri Panchul <yuri@panchul.com>
2025-05-31 00:01:03 -0700

    The baseline for hackathon

---
+++ hackathon/4_d_flip_flop/hackathon_top.sv
@@ -0,0 +1,72 @@
+// Board configuration: tang_nano_9k_lcd_480_272_tm1638_hackathon
+// This module uses few parameterization and relaxed typing rules
+
+module hackathon_top
+(
+    input  logic       clock,
+    input  logic       slow_clock,
+    input  logic       reset,
+
+    input  logic [7:0] key,
+    output logic [7:0] led,
+
+    // A dynamic seven-segment display
+
+    output logic [7:0] abcdefgh,
+    output logic [7:0] digit,
+
+    // LCD screen interface
+
+    input  logic [8:0] x,
+    input  logic [8:0] y,
+
+    output logic [4:0] red,
+    output logic [5:0] green,
+    output logic [4:0] blue,
+
+    inout  logic [3:0] gpio
+);
+
+    wire any_key = | key;
+
+    assign led [7] = slow_clock;
+
+    d_flip_flop i0
+    (
+        .clock   ( slow_clock ),
+        .d       ( any_key    ),
+        .q       ( led [0]    )
+    );
+
+    d_flip_flop_sync_reset i1
+    (
+        .clock   ( slow_clock ),
+        .reset,
+        .d       ( any_key    ),
+        .q       ( led [1]    )
+    );
+
+    d_flip_flop_async_reset i2
+    (
+        .clock   ( slow_clock ),
+        .reset,
+        .d       ( any_key    ),
+        .q       ( led [2]    )
+    );
+
+    //  Pulse generator, 50 times a second
+
+    logic enable;
+    strobe_gen # (.clk_mhz (27), .strobe_hz (1))
+    i_strobe_gen (clock, reset, enable);
+
+    d_flip_flop_sync_reset_and_enable i3
+    (
+        .clock   ( clock      ),  // Note this is not a slow_clock
+        .reset,
+        .enable,
+        .d       ( any_key    ),
+        .q       ( led [3]    )
+    );
+
+endmodule
---
+++ hackathon/5_binary_counter/hackathon_top.sv
@@ -0,0 +1,85 @@
+// Board configuration: tang_nano_9k_lcd_480_272_tm1638_hackathon
+// This module uses few parameterization and relaxed typing rules
+
+module hackathon_top
+(
+    input  logic       clock,
+    input  logic       slow_clock,
+    input  logic       reset,
+
+    input  logic [7:0] key,
+    output logic [7:0] led,
+
+    // A dynamic seven-segment display
+
+    output logic [7:0] abcdefgh,
+    output logic [7:0] digit,
+
+    // LCD screen interface
+
+    input  logic [8:0] x,
+    input  logic [8:0] y,
+
+    output logic [4:0] red,
+    output logic [5:0] green,
+    output logic [4:0] blue,
+
+    inout  logic [3:0] gpio
+);
+
+    // Exercise 1: Free running counter.
+    // How do you change the speed of LED blinking?
+    // Try different bit slices to display.
+
+    logic [31:0] counter;
+
+    always_ff @ (posedge clock)
+        if (reset)
+            counter <= 0;
+        else
+            counter <= counter + 1;
+
+    assign led = counter [31:24];  // Try to put [23:16] here
+
+    // assign led = counter >> 20;  // Try alternative way to shift the value
+
+    // Try to add "if (key)" after "else".
+
+    // Exercise 2: Key-controlled counter.
+    // Comment out the code above.
+    // Uncomment and synthesize the code below.
+    // Press the key to see the counter incrementing.
+    //
+    // Change the design, for example:
+    //
+    // 1. One key is used to increment, another to decrement.
+    //
+    // 2. Two counters controlled by different keys
+    // displayed in different groups of LEDs.
+
+    // START_SOLUTION
+
+    /*
+
+    wire k = | key;  // Any key is on
+
+    logic k_previous;
+
+    always_ff @ (posedge clock)
+        if (reset)
+            k_previous <= 0;
+        else
+            k_previous <= k;
+
+    wire k_pressed = k & ~ k_previous;
+
+    always_ff @ (posedge clock)
+        if (reset)
+            led <= 0;
+        else if (k_pressed)
+            led <= led + 1;
+    */
+
+    // END_SOLUTION
+
+endmodule
