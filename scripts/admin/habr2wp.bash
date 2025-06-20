6c7b77577f4846834a34b2fb851cf58e9bd3e6d6
Yuri Panchul <yuri@panchul.com>
2025-06-20 10:53:59 -0700

    improved the habr script

--- scripts/admin/habr2wp.bash
+++ scripts/admin/habr2wp.bash
@@ -4,18 +4,15 @@ set -Eeuo pipefail  # See the meaning in scripts/README.md
 # set -x  # Print each command
 
 sed -E \
-    -e 's/<div class="floating-image">//g'  \
-    -e 's/width="[0-9]+"/width="800"/g'  \
-    -e 's/ width=""//g'  \
-    -e 's/ height="[0-9]+"//g'  \
-    -e 's/ data-src="[^"]+"//g'  \
-    -e 's/<\/div>//g'  \
-    -e 's/<figure class="float bordered ">//g'  \
-    -e 's/<figure class="float full-width ">//g'  \
-    -e 's/<figure class="full-width ">//g'  \
-    -e 's/<\/figure>//g'  \
+    -e 's/<\/?(div|figure|span)[^>]*>//g'  \
+    -e 's/<img src="([^"]+)"[^>]*>/<a href="\1"><img width=800 src="\1" \/><\/a>/g'  \
+    -e 's/<img src="([^"]+)"[^>]*$/<a href="\1"><img width=800 src="\1" \/><\/a>/g'  \
+    -e 's/ +https:\/\/habrastorage.org\/[^>]+>//g'  \
     -e 's/rel="noopener noreferrer nofollow"/rel="noopener"/g'  \
     habr.html > wp.html
 
-sed -E -i 's/<img [^>]+>/\n&\n/g' wp.html
+sed -E -i 's/<a [^>]+><img [^>]+><\/a>/\n&\n/g' wp.html
 sed -E -i '/^$/N;/^\n$/D' wp.html
+
+
+#     -e 's/<\/?(div|figure|span)[^>]*>//g'  \
