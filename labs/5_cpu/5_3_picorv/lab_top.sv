cc5561f29f60f96f376576a8cf61fa8208705e40
Aleksandr Ryabov <s4.ryabow@yandex.ru>
2025-06-23 21:13:20 +0300

    Minimal version with integrated picorv

---
+++ labs/5_cpu/5_3_picorv/lab_top.sv
@@ -0,0 +1,121 @@
+`include "config.svh"
+
+module lab_top
+# (
+    parameter  clk_mhz       = 50,
+               w_key         = 4,
+               w_sw          = 8,
+               w_led         = 8,
+               w_digit       = 8,
+               w_gpio        = 100,
+
+               screen_width  = 640,
+               screen_height = 480,
+
+               w_red         = 4,
+               w_green       = 4,
+               w_blue        = 4,
+
+               w_x           = $clog2 ( screen_width  ),
+               w_y           = $clog2 ( screen_height )
+)
+(
+    input                        clk,
+    input                        slow_clk,
+    input                        rst,
+
+    // Keys, switches, LEDs
+
+    input        [w_key   - 1:0] key,
+    input        [w_sw    - 1:0] sw,
+    output logic [w_led   - 1:0] led,
+
+    // A dynamic seven-segment display
+
+    output logic [          7:0] abcdefgh,
+    output logic [w_digit - 1:0] digit,
+
+    // Graphics
+
+    input        [w_x     - 1:0] x,
+    input        [w_y     - 1:0] y,
+
+    output logic [w_red   - 1:0] red,
+    output logic [w_green - 1:0] green,
+    output logic [w_blue  - 1:0] blue,
+
+    // Microphone, sound output and UART
+
+    input        [         23:0] mic,
+    output       [         15:0] sound,
+
+    input                        uart_rx,
+    output                       uart_tx,
+
+    // General-purpose Input/Output
+
+    inout        [w_gpio  - 1:0] gpio
+);
+
+    //------------------------------------------------------------------------
+
+    assign led        = '0;
+    assign abcdefgh   = '0;
+    assign digit      = '0;
+    assign red        = '0;
+    assign green      = '0;
+    assign blue       = '0;
+    assign sound      = '0;
+    assign uart_tx    = '1;
+
+    //------------------------------------------------------------------------
+    wire                trap;
+    wire                mem_valid;
+	wire                mem_instr;
+	reg                 mem_ready;
+	wire [31:0]         mem_addr;
+	wire [31:0]         mem_wdata;
+	wire [3:0]          mem_wstrb;
+	reg  [31:0]         mem_rdata;
+
+
+	picorv32 picorv (
+		.clk         (clk       ),
+		.resetn      (rst       ),
+		.trap        (trap      ),
+		.mem_valid   (mem_valid ),
+		.mem_instr   (mem_instr ),
+		.mem_ready   (mem_ready ),
+		.mem_addr    (mem_addr  ),
+		.mem_wdata   (mem_wdata ),
+		.mem_wstrb   (mem_wstrb ),
+		.mem_rdata   (mem_rdata )
+	);
+
+	reg [31:0] memory [0:255];
+
+	initial begin
+		memory[0] = 32'h 3fc00093; //       li      x1,1020
+		memory[1] = 32'h 0000a023; //       sw      x0,0(x1)
+		memory[2] = 32'h 0000a103; // loop: lw      x2,0(x1)
+		memory[3] = 32'h 00110113; //       addi    x2,x2,1
+		memory[4] = 32'h 0020a023; //       sw      x2,0(x1)
+		memory[5] = 32'h ff5ff06f; //       j       <loop>
+	end
+
+	always @(posedge clk) begin
+		mem_ready <= 0;
+		if (mem_valid && !mem_ready) begin
+			if (mem_addr < 1024) begin
+				mem_ready <= 1;
+				mem_rdata <= memory[mem_addr >> 2];
+				if (mem_wstrb[0]) memory[mem_addr >> 2][ 7: 0] <= mem_wdata[ 7: 0];
+				if (mem_wstrb[1]) memory[mem_addr >> 2][15: 8] <= mem_wdata[15: 8];
+				if (mem_wstrb[2]) memory[mem_addr >> 2][23:16] <= mem_wdata[23:16];
+				if (mem_wstrb[3]) memory[mem_addr >> 2][31:24] <= mem_wdata[31:24];
+			end
+			/* add memory-mapped IO here */
+		end
+	end
+
+endmodule
