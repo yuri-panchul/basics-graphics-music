e8e92d2ae52b0778e518d541ea373919fc7cf68c
Yuri Panchul <yuri@panchul.com>
2025-07-31 08:19:44 -0700

    Add a directory for screenshot

---
+++ book/03_static_graphics_for_screenshot/static_graphics.sv
@@ -0,0 +1,21 @@
+module static_graphics
+(
+    input  [9:0] x,
+    input  [9:0] y,
+
+    output logic red,
+    output logic green,
+    output logic blue
+);
+
+    always_comb
+    begin
+        red   = 0;
+        green = 0;
+        blue  = 0;
+
+        if (x > 100 & x < 300 & y > 50 & y < 100)
+            red = 31;
+    end
+
+endmodule
