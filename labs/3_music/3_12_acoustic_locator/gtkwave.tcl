d8299f554102dd8aeba6a2488943f4874bdd0c44
Vadim Ostrikov <ovip7@mail.ru>
2025-12-23 00:47:15 +0400

    Added acoustic locator lab

---
+++ labs/3_music/3_12_acoustic_locator/gtkwave.tcl
@@ -0,0 +1,67 @@
+# gtkwave::loadFile "dump.vcd"
+
+set all_signals [list]
+
+lappend all_signals tb.sound_1
+lappend all_signals tb.sound_2
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[10\].delay\[38:25\]
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[10\].delay_2
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[10\].out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[10\].abs
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[10\].ema
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[0\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[1\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[2\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[3\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[4\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[5\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[6\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[7\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[8\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[9\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[10\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[11\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[12\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[13\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[14\].rms_out
+lappend all_signals tb.i_lab_top.i_locator.i_correlator_h\[15\].rms_out
+
+set num_added [ gtkwave::addSignalsFromList $all_signals ]
+
+gtkwave::highlightSignalsFromList "tb.sound_1\[23:0\]"
+gtkwave::highlightSignalsFromList "tb.sound_2\[23:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[10\].delay\[38:25\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[10\].delay_2\[12:0\]"
+gtkwave::/Edit/Data_Format/Analog/Step
+gtkwave::/Edit/Data_Format/Signed_Decimal
+gtkwave::/Edit/UnHighlight_All
+
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[10\].out\[13:0\]"
+gtkwave::/Edit/Data_Format/Analog/Step
+gtkwave::/Edit/Data_Format/Signed_Decimal
+gtkwave::/Edit/Insert_Analog_Height_Extension
+gtkwave::/Edit/UnHighlight_All
+
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[10\].abs\[13:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[10\].ema\[19:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[0\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[1\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[2\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[3\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[4\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[5\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[6\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[7\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[8\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[9\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[10\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[11\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[12\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[13\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[14\].rms_out\[12:0\]"
+gtkwave::highlightSignalsFromList "tb.i_lab_top.i_locator.i_correlator_h\[15\].rms_out\[12:0\]"
+gtkwave::/Edit/Data_Format/Analog/Step
+gtkwave::/Edit/Data_Format/Decimal
+gtkwave::/Edit/UnHighlight_All
+
+gtkwave::/Time/Zoom/Zoom_Full
