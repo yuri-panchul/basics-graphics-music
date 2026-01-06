44ec084e217beeb66bd5eee43799d066ac7e94b8
Dmitry Petrenko <zlobec@gmail.com>
2026-01-06 18:48:39 +0300

    Diract interrupt for local and external interrupts example

--- labs/5_cpu/5_4_yrv_plus/cpu/yrv_mcu.v
+++ labs/5_cpu/5_4_yrv_plus/cpu/yrv_mcu.v
@@ -31,18 +31,6 @@
 
 /* processor                                                                               */
 `include "yrv_top.vh"
-/* serial receive                                                                          */
-//`include "serial_rx.vh"
-/* serial transmit                                                                         */
-//`include "serial_tx.vh"
-/* serial port                                                                             */
-`include "serial_top.vh"
-
-`ifdef INSTANCE_MEM
-/* instantiated memory                                                                     */
-`include "inst_mem.vh"
-`endif
-
 
 // For real boards
 `ifndef SIMULATION
@@ -72,6 +60,7 @@ module yrv_mcu
                  , mem_ready, mem_rdata, mem_lock, mem_write, mem_trans, mem_ble,
                  mem_addr, mem_wdata, extra_debug_data
                  `endif
+                 , li_req
   );
 
   input         clk;                                       /* cpu clock                    */
@@ -107,6 +96,8 @@ module yrv_mcu
 
   output [31:0] extra_debug_data;                          /* extra debug data unconnected */
 `endif
+  input   [15:0] li_req;                                    /* local int requests           */
+
 
   /*****************************************************************************************/
   /* signal declarations                                                                   */
@@ -129,7 +120,6 @@ module yrv_mcu
   wire    [1:0] mem_trans;                                 /* memory transfer type         */
   wire    [3:0] mem_ble;                                   /* memory byte lane enables     */
   wire    [7:0] rx_rdata;                                  /* receive data buffer          */
-  wire   [15:0] li_req;                                    /* local int requests           */
   wire   [15:0] port7_dat;                                 /* i/o port                     */
   wire   [31:0] mcu_rdata;                                 /* system memory read data      */
   wire   [31:0] mem_addr;                                  /* memory address               */
@@ -184,7 +174,7 @@ module yrv_mcu
   /*****************************************************************************************/
   assign bus_32    = 1'b1;
   assign mem_ready = 1'b1;
-  assign li_req    = {12'h0, bufr_empty, bufr_done, bufr_full, bufr_ovr};
+  ;
 
   /*****************************************************************************************/
   /* processor                                                                             */
@@ -415,14 +405,6 @@ initial $readmemh("code_demo.mem8", mcu_mem);
   /*****************************************************************************************/
   /* serial port                                                                           */
   /*****************************************************************************************/
-  serial_top SERIAL ( .bufr_done(bufr_done), .bufr_empty(bufr_empty), .bufr_full(bufr_full),
-                      .bufr_ovr(bufr_ovr), .rx_rdata(rx_rdata), .ser_clk(ser_clk),
-                      .ser_txd(ser_txd), .cks_mode(port6_reg[0]), .clkp(clk),
-                      .div_rate(port6_reg[15:4]), .ld_wdata(ld_wdata), .rd_rdata(rd_rdata),
-                      .s_reset(port6_reg[3]), .ser_rxd(ser_rxd), .tx_wdata(mem_wdata[7:0]) );
-
-  assign ld_wdata  = io_wr_reg && port76_dec && mem_ble_reg[2] && mem_ready;
-  assign rd_rdata  = io_rd_reg && port76_dec && mem_ble_reg[2] && mem_ready;
-  assign port7_dat = {4'h0, bufr_empty, bufr_done, bufr_full, bufr_ovr, rx_rdata};
-
+  // You cam add own serial port on port7
+  
   endmodule
