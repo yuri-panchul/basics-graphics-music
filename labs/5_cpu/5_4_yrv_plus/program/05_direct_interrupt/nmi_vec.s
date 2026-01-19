9284b42d0761db0b2584edb139f9adb5b4f7ecf0
Dmitry Petrenko <zlobec@gmail.com>
2026-01-19 18:57:08 +0300

    Fix

--- labs/5_cpu/5_4_yrv_plus/program/05_direct_interrupt/nmi_vec.s
+++ labs/5_cpu/5_4_yrv_plus/program/05_direct_interrupt/nmi_vec.s
@@ -2,20 +2,20 @@
 .global nmi_vec
 .global run_display
 
-# Обработчик NMI-вектора
+# NMI Trap handler
 nmi_vec:
 
-    csrrw a0, mscratch, a0         # сохраняем регистр a0 и получаем указатель
-    sw ra, 0(a0)                   # сохраняем RA
-    sw s0, 4(a0)                   # сохраняем s0
-    sw s1, 8(a0)                   # сохраняем s1
-    addi sp, sp, -16               # резервируем пространство на стеке
+    csrrw a0, mscratch, a0        
+    sw ra, 0(a0)                   
+    sw s0, 4(a0)                   
+    sw s1, 8(a0)                   
+    addi sp, sp, -16               
 
-    jal ra, run_display            # вызываем run_display()
+    jal ra, run_display            
 
-    lw ra, 0(a0)                   # восстанавливаем RA
-    lw s0, 4(a0)                   # восстанавливаем s0
-    lw s1, 8(a0)                   # восстанавливаем s1
-    addi sp, sp, 16                # освобождаем пространство на стеке
-    csrrw a0, mscratch, a0         # восстанавливаем a0 и указатель
-    ret                           # выход из обработчика прерывания
+    lw ra, 0(a0)                   
+    lw s0, 4(a0)                 
+    lw s1, 8(a0)                  
+    addi sp, sp, 16               
+    csrrw a0, mscratch, a0        
+    mret                           
