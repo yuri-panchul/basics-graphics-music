2ee875c0b2a172ffb59eb4bdb22a5c12669a90a5
Jason Yang <yanghuaxuan@gmail.com>
2026-04-12 21:25:41 +0000

    add Nix flakes for Librelane flow

---
+++ flake.nix
@@ -0,0 +1,89 @@
+{
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-cache.fossi-foundation.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-cache.fossi-foundation.org:3+K59iFwXqKsL7BNu6Guy0v+uTlwsxYQxjspXzqLYQs="
+    ];
+  };
+
+  inputs = {
+    librelane.url = "github:librelane/librelane/dev";
+  };
+
+  outputs =
+    {
+      self,
+      librelane,
+      ...
+    }:
+    let
+      nix-eda = librelane.inputs.nix-eda;
+      devshell = librelane.inputs.devshell;
+      nixpkgs = nix-eda.inputs.nixpkgs;
+      lib = nixpkgs.lib;
+    in
+    {
+      # Outputs
+      legacyPackages = nix-eda.forAllSystems (
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [
+            nix-eda.overlays.default
+            devshell.overlays.default
+            librelane.overlays.default
+            (final: prev: {
+              klayout = prev.klayout.override {
+                version = "0.30.4";
+                sha256 = "sha256-eL1RjqZpZI3+20ax+NsQ3qoOPzdm4k6KJwW+Rn0wg8Y=";
+              };
+            })
+          ];
+        }
+      );
+
+      packages = nix-eda.forAllSystems (system: {
+        inherit (self.legacyPackages.${system}.python3.pkgs) ;
+      });
+
+      devShells = nix-eda.forAllSystems (
+        system:
+        let
+          pkgs = (self.legacyPackages.${system});
+          callPackage = lib.callPackageWith pkgs;
+        in
+        {
+          default = pkgs.librelane-shell.override ({
+            extra-packages = with pkgs; [
+              # Utilities
+              gnumake
+              gnugrep
+              gawk
+
+              # Simulation
+              iverilog
+              verilator
+
+              # Waveform viewing
+              gtkwave
+              surfer
+
+              # For RARS
+              javaPackages.compiler.temurin-bin.jre-25
+            ];
+
+            extra-python-packages =
+              ps: with ps; [
+                # Verification
+                cocotb
+
+                # For KLayout Python DRC runner
+                docopt
+              ];
+          });
+        }
+      );
+    };
+}
