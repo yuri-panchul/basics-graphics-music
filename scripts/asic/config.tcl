701b12317409e76311a3b61b46b9d6d2fae3dff8
Jason Yang <yanghuaxuan@gmail.com>
2026-04-12 21:30:02 +0000

    update asic flow scripts

--- scripts/asic/config.tcl
+++ scripts/asic/config.tcl
@@ -6,9 +6,11 @@ set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.{v,sv}]
 set ::env(CLOCK_PORT) "clk"
 
 set ::env(FP_SIZING) "absolute"
-set ::env(DIE_AREA) {0 0 100 100}
+set ::env(DIE_AREA) {0 0 1000 1000}
 
 set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
+
+set ::env(ERROR_ON_LINTER_WARNINGS) 0
 
 if { [file exists $filename] == 1} {
   source $filename
