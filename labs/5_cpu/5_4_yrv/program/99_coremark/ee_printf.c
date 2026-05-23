81996edd65d0e31064e5dbb7d1997281f3d221ca
Yuri Panchul <yuri@panchul.com>
2026-05-23 08:53:53 -0700

    Cosmetic cleanup

--- labs/5_cpu/5_4_yrv/program/99_coremark/ee_printf.c
+++ labs/5_cpu/5_4_yrv/program/99_coremark/ee_printf.c
@@ -663,17 +663,17 @@ void
 uart_send_char(char c)
 {
 //#error "You must implement the method uart_send_char to use this file!\n";
-    /*	Output of a char to a UART usually follows the following model:
-            Wait until UART is ready
-            Write char to UART
-            Wait until UART is done
+    /* Output of a char to a UART usually follows the following model:
+       Wait until UART is ready
+       Write char to UART
+       Wait until UART is done
 
-            Or in code:
-            while (*UART_CONTROL_ADDRESS != UART_READY);
-            *UART_DATA_ADDRESS = c;
-            while (*UART_CONTROL_ADDRESS != UART_READY);
+       Or in code:
+       while (*UART_CONTROL_ADDRESS != UART_READY);
+       *UART_DATA_ADDRESS = c;
+       while (*UART_CONTROL_ADDRESS != UART_READY);
 
-            Check the UART sample code on your platform or the board
+       Check the UART sample code on your platform or the board
        documentation.
     */
 }
