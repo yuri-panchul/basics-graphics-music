4dd00e7d55a3eb98e718ab92de0c68706a2de4ba
Dmitry Petrenko <zlobec@gmail.com>
2026-05-28 18:00:40 +0300

    MMIO Structs with union uint32_t

--- labs/5_cpu/5_4_yrv/program/01_helo_text/memory_mapped_registers.h
+++ labs/5_cpu/5_4_yrv/program/01_helo_text/memory_mapped_registers.h
@@ -17,24 +17,157 @@
 #define MMIO_KEY_SW  MMIO ( MMIO_KEY_SW_ADDR )
 #define MMIO_SERIAL  MMIO ( MMIO_SERIAL_ADDR )
 
+// # 0x00010000 port0 = {8'bxxxxxxxx, RDP, RA, RB, RC, RD, RE, RF, RG}
+// # 0x00010002 port1 = {4'bxxxx, C46, C45, C43, C42, 4'bxxxx, AN4, AN3, AN2, AN1}
 typedef struct
 {
-    unsigned
-    reserved : 8,
-    rdp      : 1,
-    ra       : 1,
-    rb       : 1,
-    rc       : 1,
-    rd       : 1,
-    re       : 1,
-    rf       : 1,
-    rg       : 1;
+    union {
+        uint32_t io;
+        struct {
+            unsigned
+            free1     : 4, //TODO
+            c46       : 1,
+            c45       : 1,
+            c43       : 1,
+            c42       : 1,
+            free2     : 4, //TODO
+            an4       : 1,
+            an3       : 1,
+            an2       : 1,
+            an1       : 1,
+            free3     : 8, //TODO
+            rdp       : 1,
+            ra        : 1,
+            rb        : 1,
+            rc        : 1,
+            rd        : 1,
+            re        : 1,
+            rf        : 1,
+            rg        : 1;
+        } bits;
+    };
 }
 mmio_7seg_t;
 
+// # 0x00010004 port2 = L[16:1]
+// # 0x00010006 port3 = {CLR_EI, 1'bx, INIT, ECALL, NMI, LINT, INT, EXCEPT, L[24:17]}
 typedef struct
 {
-    mmio_7seg_t seven_seg;
+    union {
+        uint32_t io;
+        struct {
+            unsigned
+            l15       : 1,
+            l14       : 1,
+            l13       : 1,
+            l12       : 1,
+            l11       : 1,
+            l10       : 1,
+            l9        : 1,
+            l8        : 1,
+            l7        : 1,
+            l6        : 1,
+            l5        : 1,
+            l4        : 1,
+            l3        : 1,
+            l2        : 1,
+            l1        : 1,
+            l0        : 1,
+            clr_ei    : 1,
+            free      : 1, //TODO
+            init      : 1,
+            ecall     : 1,
+            nmi       : 1,
+            lint      : 1,
+            internal  : 4,
+            except    : 1,
+            l24       : 1,
+            l23       : 1,
+            l22       : 1,
+            l21       : 1,
+            l20       : 1,
+            l19       : 1,
+            l18       : 1,
+            l17       : 1,
+            l16       : 1;
+        } bits;
+    };
+}
+mmio_led_t;
+
+// # 0x00010008 port4 = DIP[16:1]
+// # 0x0001000a port5 = {C9, C8, C6, S5, S4, S3, S2, S1, DIP[24:17]}
+typedef struct
+{
+    union {
+        uint32_t io;
+        struct {
+            unsigned
+            dip15       : 1,
+            dip14       : 1,
+            dip13       : 1,
+            dip12       : 1,
+            dip11       : 1,
+            dip10       : 1,
+            dip9        : 1,
+            dip8        : 1,
+            dip7        : 1,
+            dip6        : 1,
+            dip5        : 1,
+            dip4        : 1,
+            dip3        : 1,
+            dip2        : 1,
+            dip1        : 1,
+            dip0        : 1,
+            c9          : 1,
+            c8          : 1, 
+            c6          : 1,
+            s5          : 1,
+            s4          : 1,
+            s3          : 1,
+            s2          : 1,
+            s1          : 1,
+            dip24       : 1,
+            dip23       : 1,
+            dip22       : 1,
+            dip21       : 1,
+            dip20       : 1,
+            dip19       : 1,
+            dip18       : 1,
+            dip17       : 1,
+            dip16       : 1;
+        } bits;
+    };
+}
+mmio_key_sw_t;
+
+// # 0x0001000c port6 = {DIV_RATE, S_RESET, 3'bxxx}
+// # 0x0001000e port7 = {4'bxxxx, EMPTY, DONE, FULL, OVR, SER_DATA}
+typedef struct
+{
+    union {
+        uint32_t io;
+        struct {
+            unsigned
+            div_rate    : 12,
+            s_reset     :  1,
+            free        :  7,
+            empty       :  1,
+            done        :  1,
+            full        :  1,
+            ovr         :  1,
+            ser_data    :  8;
+        } bits;
+    };
+}
+mmio_serial_t;
+
+typedef struct
+{
+    mmio_7seg_t    seven_seg;
+    mmio_led_t     led;
+    mmio_key_sw_t  key_sw;
+    mmio_serial_t  serial;
 }
 mmio_t;
 
