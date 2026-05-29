681cfef8de2c26f9ee059e02520cf428b12046fb
Yuri Panchul <yuri@panchul.com>
2026-05-29 09:10:17 -0700

    linked script and MMIO

--- labs/5_cpu/5_4_yrv/program/01_helo_text/main.c
+++ labs/5_cpu/5_4_yrv/program/01_helo_text/main.c
@@ -8,28 +8,30 @@ void main ()
 //    mmio.seven_seg.ra = 1;
 //    MMIO_7SEG         = 123;
 
-    mmio.seven_seg.bits.free1 = 12;
-    mmio.seven_seg.bits.rdp      = 0;
-    mmio.seven_seg.bits.ra       = 1;
-    mmio.seven_seg.bits.rb       = 0;
-    mmio.seven_seg.bits.rc       = 1;
-    mmio.seven_seg.bits.rd       = 1;
-    mmio.seven_seg.bits.re       = 0;
-    mmio.seven_seg.bits.rf       = 0;
-    mmio.seven_seg.bits.rg       = 1;
+//    mmio.seven_seg.free1 = 12;
+    mmio.seven_seg.rdp      = 0;
+/*
+    mmio.seven_seg.ra       = 1;
+    mmio.seven_seg.rb       = 0;
+    mmio.seven_seg.rc       = 1;
+    mmio.seven_seg.rd       = 1;
+    mmio.seven_seg.re       = 0;
+    mmio.seven_seg.rf       = 0;
+    mmio.seven_seg.rg       = 1;
 
     mmio_7seg_t seg;
 
-    seg.bits.free1 = 12;
-    seg.bits.rdp      = 0;
-    seg.bits.ra       = 1;
-    seg.bits.rb       = 0;
-    seg.bits.rc       = 1;
-    seg.bits.rd       = 1;
-    seg.bits.re       = 0;
-    seg.bits.rf       = 0;
-    seg.bits.rg       = 1;
+//    seg.free1 = 12;
+    seg.rdp      = 0;
+    seg.ra       = 1;
+    seg.rb       = 0;
+    seg.rc       = 1;
+    seg.rd       = 1;
+    seg.re       = 0;
+    seg.rf       = 0;
+    seg.rg       = 1;
 
     mmio.seven_seg = seg;
+*/
 }
 
