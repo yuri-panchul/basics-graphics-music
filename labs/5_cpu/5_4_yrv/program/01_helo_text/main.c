68d8b2740474d1725cefd98bd1dfe874a835a516
Dmitry Petrenko <zlobec@gmail.com>
2026-05-29 17:32:15 +0300

    delay function fro led blinking

--- labs/5_cpu/5_4_yrv/program/01_helo_text/main.c
+++ labs/5_cpu/5_4_yrv/program/01_helo_text/main.c
@@ -2,6 +2,24 @@
 // experience with HEX on differents boards
 
 #include "memory_mapped_registers.h"
+#define MTIME_CLK_FREQ 27000000
+#define TICKS_PER_MS   (MTIME_CLK_FREQ / 1000)
+
+// Macros for reading and writing the control and status registers (CSRs)
+// Form Digital Design and Computer Architecture, RISC-V Edition 542.e29
+#define read_csr(reg) ({ unsigned long __tmp; \
+asm volatile ("csrr %0, " #reg : "= r"(__tmp)); \
+__tmp; })
+
+#define write_csr(reg, val) ({ \
+asm volatile ("csrw " #reg ", %0" :: "rK"(val)); })
+
+void delay(int ms) {
+    unsigned long start_time = read_csr(time);
+    unsigned long wait_time = (unsigned long)ms * TICKS_PER_MS;
+    while ((read_csr(time) - start_time) < wait_time); // Wait until time is reached. Unsigned help with overload
+}
+
 
 void main ()
 {
@@ -20,6 +38,7 @@ void main ()
 
     mmio_7seg_t seg;
 
+    delay(10);
     seg.bits.free1 = 12;
     seg.bits.rdp      = 0;
     seg.bits.ra       = 1;
@@ -31,5 +50,7 @@ void main ()
     seg.bits.rg       = 1;
 
     mmio.seven_seg = seg;
+
+    
 }
 
