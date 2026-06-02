afa00312f522fb8fc06ac223e1dd59243ad821e9
Yuri Panchul <yuri@panchul.com>
2026-06-02 08:29:21 -0700

    Fixing multitasking example

--- labs/5_cpu/5_4_yrv/program/05_yuri_2/bgm_pico_threads.h
+++ labs/5_cpu/5_4_yrv/program/05_yuri_2/bgm_pico_threads.h
@@ -3,6 +3,8 @@
 
 #define MAX_THREADS  8
 
+#ifndef __ASSEMBLER__
+
 typedef void (* pointer_to_void_function_no_arguments) ();
 
 extern bool define_thread (void (* func) ());
@@ -10,4 +12,5 @@ extern bool define_thread (pointer_to_void_function_no_arguments func);
 
 extern void start_running_threads ();
 
-#endif
+#endif  // #ifndef __ASSEMBLER__
+#endif  // #ifndef BGM_PICO_THREADS_H
