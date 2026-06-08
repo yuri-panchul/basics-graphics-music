6593463b3ba8958c43d54fc5ba5d21942e606e29
Yuri Panchul <yuri@panchul.com>
2026-06-07 21:03:14 -0700

    Added a baseline for the third variant of multitasking

---
+++ labs/5_cpu/5_4_yrv/program/05_yuri_3/bgm_femto_threads.h
@@ -0,0 +1,16 @@
+#ifndef BGM_FEMTO_THREADS_H
+#define BGM_FEMTO_THREADS_H
+
+#define MAX_THREADS  8
+
+#ifndef __ASSEMBLER__
+
+typedef void (* pointer_to_void_function_no_arguments) ();
+
+extern bool define_thread (void (* func) ());
+extern bool define_thread (pointer_to_void_function_no_arguments func);
+
+extern void start_running_threads ();
+
+#endif  // #ifndef __ASSEMBLER__
+#endif  // #ifndef BGM_FEMTO_THREADS_H
