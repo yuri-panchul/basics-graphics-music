3c784fcd8a58b0da542ac11df1e6f145ffd6f575
Yuri Panchul <yuri@panchul.com>
2026-06-17 21:23:04 -0700

    Added dir for next yuri version 5

---
+++ labs/5_cpu/5_4_yrv/program/05_yuri_5/main.c
@@ -0,0 +1,38 @@
+#include "bgm_femto_threads.h"
+#include "memory_mapped_registers.h"
+
+void f ()
+{
+    for (;;);
+}
+
+int counter_1 = 0;
+
+void thread_1 ()
+{
+    counter_1 ++;
+}
+
+int counter_2 = 0;
+
+void thread_2 ()
+{
+    counter_2 ++;
+}
+
+int counter_3 = 0;
+
+void thread_3 ()
+{
+    counter_3 ++;
+}
+
+void main ()
+{
+    define_thread (thread_1);
+    define_thread (thread_2);
+    define_thread (thread_3);
+
+    start_running_threads ();
+    for (;;);
+}
