b2f8a9ea08d42350676c83dfe2c1f02cbef54a88
Yuri Panchul <yuri@panchul.com>
2026-06-27 13:26:53 -0700

    IO for LED

--- labs/5_cpu/5_4_yrv/program/10_femto_threads_stackless_threads/main.c
+++ labs/5_cpu/5_4_yrv/program/10_femto_threads_stackless_threads/main.c
@@ -19,9 +19,23 @@ void thread_3 ()
 {
     // Output to LED
 
+    #if defined (METHOD1)
+
+    mmio.led.b.l0 = (counter_1 & 1) != 0;
+    mmio.led.b.l1 = (counter_1 & 2) != 0;
+    mmio.led.b.l2 = (counter_1 & 4) != 0;
+    mmio.led.b.l3 = (counter_1 & 8) != 0;
+
+    mmio.led.b.l4 = (counter_2 & 1) != 0;
+    mmio.led.b.l5 = (counter_2 & 2) != 0;
+    mmio.led.b.l6 = (counter_2 & 4) != 0;
+    mmio.led.b.l7 = (counter_2 & 8) != 0;
+
+    #elif defined (METHOD2)
+
     mmio_led_t led;
 
-    led.w = 0;
+    led.w = mmio.led.w;
 
     led.b.l0 = (counter_1 & 1) != 0;
     led.b.l1 = (counter_1 & 2) != 0;
@@ -35,8 +49,32 @@ void thread_3 ()
 
     mmio.led = led;
 
-    led.f.l24_0 = ((counter_1 & 0xf) << 4) | (counter_2 & 0xf);
+    #elif defined (METHOD3)
+
+    mmio_led_t led;
+
+    int c1 = counter_1;
+    int c2 = counter_2;
+
+    led.w = mmio.led.w;
+
+    led.b.l0 = (c1 & 1) != 0;
+    led.b.l1 = (c1 & 2) != 0;
+    led.b.l2 = (c1 & 4) != 0;
+    led.b.l3 = (c1 & 8) != 0;
+
+    led.b.l4 = (c2 & 1) != 0;
+    led.b.l5 = (c2 & 2) != 0;
+    led.b.l6 = (c2 & 4) != 0;
+    led.b.l7 = (c2 & 8) != 0;
+
     mmio.led = led;
+
+    #else
+
+    mmio.led.f.l24_0 = ((counter_1 & 0xf) << 4) | (counter_2 & 0xf);
+
+    #endif
 }
 
 void main ()
