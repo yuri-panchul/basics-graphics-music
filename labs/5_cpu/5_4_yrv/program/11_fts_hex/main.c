613d2a9fb38845a0e8deaf5ae84398bec918954b
Dmitry Petrenko <zlobec@gmail.com>
2026-06-28 16:29:12 +0300

    Some fix

--- labs/5_cpu/5_4_yrv/program/11_fts_hex/main.c
+++ labs/5_cpu/5_4_yrv/program/11_fts_hex/main.c
@@ -20,13 +20,16 @@ uint8_t inline char_to_hex(uint32_t c) {
     }
 }
 
-//https://forum.arduino.cc/t/divmod10-a-fast-replacement-for-10-and-10-unsigned/163586
-static inline uint32_t div10_fast(uint32_t in) {
-    register uint32_t q = (in >> 1) + (in >> 2);
-    q = q + (q >> 4);
-    q = q + (q >> 8);
-    q = q + (q >> 16);
-    return q >> 3;
+//Hacker's Delight (need review)
+static inline uint32_t div10(uint32_t in) {
+    register uint32_t triple = (in << 1) + in;                
+    register  uint32_t fifty_one = (triple << 4) + triple;     
+    register  uint32_t two_hundred_five = (fifty_one << 2) + in; 
+    return two_hundred_five >> 11;                   
+}
+
+static inline uint32_t mul10(uint32_t in) {
+    return (in << 3) + (in << 1);
 }
 
 int counter_1 = 0;
@@ -46,7 +49,7 @@ void thread_2 ()
 void thread_3 ()
 {
     half = half?0:1;
-    register uint32_t counter = div10_fast(half?counter_2:counter_1);
+    register uint32_t counter = div10(half?counter_2:counter_1);
     register uint8_t hex_code = char_to_hex(counter);
     MMIO_LED = half?LED_3:LED_1;
     MMIO_7SEG = hex_code;
@@ -54,7 +57,7 @@ void thread_3 ()
     MMIO_LED = 0x00;
     MMIO_7SEG = 0x00;
 
-    counter = (half?counter_2:counter_1) - counter;
+    counter = (half?counter_2:counter_1) - mul10(counter);
     hex_code = char_to_hex(counter);
     MMIO_LED = half?LED_2:LED_0;
     MMIO_7SEG = hex_code;
