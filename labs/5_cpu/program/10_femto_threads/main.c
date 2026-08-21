140604a1161c93cc27534adda4a177b720c6ae62
Yuri Panchul <yuri@panchul.com>
2026-08-21 10:03:40 -0700

    Draft for the core selection

---
+++ labs/5_cpu/program/10_femto_threads/main.c
@@ -0,0 +1,41 @@
+#include "memory_mapped_registers.h"
+#include "bgm_femto_threads.h"
+
+int counter_1 = 1;
+
+void thread_1 ()
+{
+    // mmio.led.f.l24_0 = 1;
+    MMIO_LED = 1;
+    counter_1 ++;
+}
+
+int counter_2 = 2;
+
+void thread_2 ()
+{
+    MMIO_LED = 2;
+    counter_2 ++;
+}
+
+void thread_3 ()
+{
+    MMIO_LED = 3;
+
+    /*
+    mmio.led.f.l24_0
+      =   (((counter_1 >> 15) & 0xf) << 4)
+        |  ((counter_2 >> 15) & 0xf);
+    */
+}
+
+void main ()
+{
+    define_thread (thread_1);
+    define_thread (thread_2);
+    define_thread (thread_3);
+
+    start_running_threads ();
+
+    for (;;);
+}
